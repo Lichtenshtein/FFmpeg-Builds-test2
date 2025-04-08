2af59b680c26fa3f45bd5d2d38dc2c0ce9702e7b
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-04-08 17:15:00 +1000

    Support LCEVC decoding support

---
+++ scripts.d/50-lcevcdec.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/v-novaltd/LCEVCdec.git"
+SCRIPT_COMMIT="bf7e0d91c969502e90a925942510a1ca8088afec"
+
+ffbuild_enabled() {
+    (( $(ffbuild_ffver) > 700 )) || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+
+    mkdir build && cd build
+
+    if [[ "$CC" != *clang* ]]; then
+        export CFLAGS="$CFLAGS -fpermissive -Wno-error=uninitialized -Wno-error=maybe-uninitialized"
+        export CXXFLAGS="$CXXFLAGS -fpermissive -Wno-error=uninitialized -Wno-error=maybe-uninitialized"
+    fi
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SHARED_LIBS=OFF ..
+
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-liblcevc_dec
+}
+
+ffbuild_unconfigure() {
+    (( $(ffbuild_ffver) > 700 )) || return 0
+    echo --disable-liblcevc_dec
+}
