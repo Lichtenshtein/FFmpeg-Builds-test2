b7c4f06ff50181f709fbad4fff05530c3c1ccb5e
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-05-14 21:28:02 +1000

    Support Apple AudioToolbox encoder feature
    Just so you know, only Windows supports; you need to install iTunes or load CoreAudioToolbox.dll to use AudioToolbox, the FFmpeg custom builds are not interrupted.
    
    Done feature request: https://github.com/MartinEesmaa/FFmpeg-Builds/issues/48
    
    Thanks, @tormento!
    
    - Martin Eesmaa

---
+++ scripts.d/50-audiotoolbox.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/cynagenautes/AudioToolboxWrapper"
+# Forked repo, original author: dantmnf
+
+ffbuild_enabled() {
+    [[ $TARGET == win* ]] || return 1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    mkdir build && cd build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" .. -G Ninja
+    ninja -j$(nproc)
+    ninja install
+
+    rm "$FFBUILD_PREFIX"/bin/atw_ldwrapper
+}
+
+ffbuild_configure() {
+    echo --enable-audiotoolbox --disable-outdev=audiotoolbox
+}
+
+ffbuild_unconfigure() {
+    echo --disable-audiotoolbox
+}
+
+ffbuild_libs() {
+    echo -lAudioToolboxWrapper
+}
