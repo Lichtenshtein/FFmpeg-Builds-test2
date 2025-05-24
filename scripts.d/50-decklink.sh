3d6edba92980392dcd0ae140db07738a70b8a7a8
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-05-24 21:26:31 +1000

    Fix decklink by disable linux target
    I will fix it later... sorry.
    
    - Martin Eesmaa

--- scripts.d/50-decklink.sh
+++ scripts.d/50-decklink.sh
@@ -5,6 +5,7 @@ SCRIPT_COMMIT="1cc63fbdb06f26b39bbb85c918d863753d969ad9"
 
 ffbuild_enabled() {
     [[ $TARGET == winarm64 ]] && return -1
+    [[ $TARGET == linux* ]] && return -1
     [[ $VARIANT == nonfree* ]] || return -1
     return 0
 }
