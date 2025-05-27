a1c85ddb02915c646ae36d2b16d6652049fa4143
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-05-27 14:15:29 +1000

    Add support of libmysofa and KLVANC features

---
+++ scripts.d/50-libmysofa.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/hoene/libmysofa"
+SCRIPT_COMMIT="dd315a8ec1fee7193d40e4a59b12c5590a4a918c"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    cd build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF ..
+
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-libmysofa
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libmysofa
+}
