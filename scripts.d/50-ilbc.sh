09785c157cb761c164199f9ea4c568dcb5343798
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-05-27 19:25:44 +1000

    Enable linux target of ilbc build script

--- scripts.d/50-ilbc.sh
+++ scripts.d/50-ilbc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/TimothyGu/libilbc"
+SCRIPT_COMMIT="6adb26d4a4e159cd66d4b4c5e411cd3de0ab6b5e"
 
 ffbuild_enabled() {
-    [[ $TARGET == win* ]] || return 1
     return 0
 }
 
@@ -12,9 +12,10 @@ ffbuild_dockerbuild() {
 
     mkdir build && cd build
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" ..
-    make -j$(nproc)
-    make install
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" .. -G Ninja
+    ninja -j$(nproc)
+    ninja install
 }
 
 ffbuild_configure() {
