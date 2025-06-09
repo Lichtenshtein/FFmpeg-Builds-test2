3891684e914fc01811b9ada5bc1d55671292bef5
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-06-09 20:54:54 +1000

    Add external support of libmad MP3 decoding
    Note: This is very old back last updated in 2004, but it has some patched files of forked repo
    
    - Martin Eesmaa

---
+++ scripts.d/50-libmad.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/sezero/libmad.git"
+SCRIPT_COMMIT="486f902c6c686eafced3450851849527e29bc7f6"
+
+ffbuild_enabled() {
+    (( $(ffbuild_ffver) > 701 )) || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    autoreconf -if
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-libmad
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libmad
+}
