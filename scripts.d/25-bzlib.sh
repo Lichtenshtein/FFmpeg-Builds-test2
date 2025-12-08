39999e7fba175c2106f2a73d4c37411f7629bfd8
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-12-08 02:28:07 +0200

    Added bzlib/bzip2 external feature support
    This allows to implement bzlib external feature to my forked repo. :)
    
    - Martin Eesmaa

---
+++ scripts.d/25-bzlib.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://gitlab.com/bzip2/bzip2.git"
+SCRIPT_COMMIT="66c46b8c9436613fd81bc5d03f63a61933a4dcc3"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    mkdir build && cd build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+    -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=OFF -DENABLE_LIB_ONLY=1 ..
+
+    make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+
+    mv "$FFBUILD_DESTPREFIX"/lib/libbz2_static.a "$FFBUILD_DESTPREFIX"/lib/libbz2.a
+}
+
+ffbuild_configure() {
+    echo --enable-bzlib
+}
+
+ffbuild_unconfigure() {
+    echo --disable-bzlib
+}
