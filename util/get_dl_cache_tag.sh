9741926563366c4977e84b4542aa67b94cf0103e
Lichtenshtein <altruumann@protonmail.com>
2026-02-08 18:26:28 +0300

    fix to find correct filenames in cache with hashes

--- util/get_dl_cache_tag.sh
+++ util/get_dl_cache_tag.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 
 # Instead of running download.sh simply hash the contents of all build scripts.
 # If change the URL or commit in any .sh script, the hash will change and the cache will be reset.
-find scripts.d variants addins -type f -name "*.sh" -print0 | xargs -0 sha256sum | sort | sha256sum | cut -d" " -f1
+find scripts.d variants addins -type f -name "*.sh" -print0 | sort -z | xargs -0 sha256sum | sha256sum | cut -d" " -f1
