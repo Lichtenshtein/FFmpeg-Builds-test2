7eb768039ad71b62ffec4dc6c9bc929c1012e62b
Lichtenshtein <altruumann@protonmail.com>
2026-02-08 18:38:11 +0300

    fix folder structure for cache calculator

--- util/get_dl_cache_tag.sh
+++ util/get_dl_cache_tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
-cd "$(dirname "$0")"
 
-# Instead of running download.sh simply hash the contents of all build scripts.
-# If change the URL or commit in any .sh script, the hash will change and the cache will be reset.
+# Переходим в корень репозитория (на один уровень выше папки util)
+cd "$(dirname "$0")/.."
+
 find scripts.d variants addins -type f -name "*.sh" -print0 | sort -z | xargs -0 sha256sum | sha256sum | cut -d" " -f1
