a4c7d02ce7cc1eaaa8c1a327fd26e147abe9466b
Lichtenshtein <altruumann@protonmail.com>
2026-02-10 14:41:16 +0300

    fix freetype builders

--- scripts.d/45-fonts/25-freetype.sh
+++ scripts.d/45-fonts/25-freetype.sh
@@ -7,26 +7,34 @@ ffbuild_enabled() {
     return 0
 }
 
+ffbuild_dockerdl() {
+    # Клонируем и сразу выкачиваем подмодули (dlg), чтобы они попали в архив
+    echo "git clone --filter=blob:none --depth=1 \"$SCRIPT_REPO\" . && git submodule update --init --recursive"
+}
+
 ffbuild_dockerbuild() {
+    # Обманываем Freetype, создавая файл-метку, что подмодули уже есть
+    # и предотвращаем вызов git в autogen.sh
+    export NOCONFIGURE=1
     ./autogen.sh
 
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
-        --without-harfbuzz
+        --host="$FFBUILD_TOOLCHAIN"
         --disable-shared
         --enable-static
+        --with-pic
+        --without-harfbuzz
+        --without-png
+        --without-zlib
+        --without-bzip2
     )
-
-    if [[ $TARGET == win* || $TARGET == linux* ]]; then
-        myconf+=(
-            --host="$FFBUILD_TOOLCHAIN"
-        )
-    else
-        echo "Unknown target"
-        return -1
-    fi
 
     ./configure "${myconf[@]}"
     make -j$(nproc)
     make install DESTDIR="$FFBUILD_DESTDIR"
+    
+    # Копируем результат в префикс
+    cp -r "$FFBUILD_DESTDIR$FFBUILD_PREFIX"/. "$FFBUILD_PREFIX"/
 }
+
