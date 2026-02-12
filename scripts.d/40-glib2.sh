4f4dd3f96ba92f5d2a003d5fc30cd53e317316b4
Lichtenshtein <altruumann@protonmail.com>
2026-02-12 21:31:01 +0300

    fix glib2 script

--- scripts.d/40-glib2.sh
+++ scripts.d/40-glib2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_REPO="https://github.com/GNOME/glib.git"
-# SCRIPT_COMMIT="6b11cae1b3bf3e9cff9485481dd1c0f7e806c361"
-SCRIPT_COMMIT="2.80.0"
+SCRIPT_COMMIT="6b11cae1b3bf3e9cff9485481dd1c0f7e806c361"
+# SCRIPT_COMMIT="2.80.0"
 
 ffbuild_enabled() {
     return 0
@@ -9,7 +9,7 @@ ffbuild_enabled() {
 
 ffbuild_dockerdl() {
     # Изменить 'v1' на 'v2', чтобы сбросить кэш загрузки
-    echo "git-mini-clone \"$SCRIPT_REPO\" \"$SCRIPT_COMMIT\" . && echo 'v12-meson-upgrade'"
+    echo "git-mini-clone \"$SCRIPT_REPO\" \"$SCRIPT_COMMIT\" . && echo 'v13-meson-upgrade'"
 }
 
 ffbuild_dockerbuild() {
@@ -19,17 +19,12 @@ ffbuild_dockerbuild() {
     # ОБНОВЛЯЕМ MESON до последней версии
     pip3 install --break-system-packages --upgrade meson
 
-    # ИНИЦИАЛИЗИРУЕМ ПОДМОДУЛИ (GVDB)
-    # Если папка пустая, git выкачает нужные исходники gvdb
-    git submodule update --init --recursive
-
-    # Пытаемся оживить подмодули. Если не выйдет - качаем GVDB вручную
+    # инициализация подмодуля gvdb
     if ! git submodule update --init --recursive; then
         echo "Submodule update failed, downloading GVDB manually..."
         rm -rf subprojects/gvdb
-        git clone --depth 1 https://github.com/GNOME/glib.git subprojects/gvdb
+        git clone --depth 1 https://github.com/GNOME/gvdb.git subprojects/gvdb
     fi
-
     # Удаляем только pcre2 из субпроектов, чтобы заставить использовать наш билд
     rm -rf subprojects/pcre2*
 
@@ -57,7 +52,6 @@ nm = '${FFBUILD_TOOLCHAIN}-gcc-nm'
 ranlib = '${FFBUILD_TOOLCHAIN}-gcc-ranlib'
 
 [properties]
-# Эти параметры критичны для кросс-компиляции GLib 2.80+
 growing_stack = false
 have_c99_snprintf = true
 have_c99_vsnprintf = true
@@ -70,25 +64,15 @@ c_link_args = [$MESON_L_ARGS]
 cpp_link_args = [$MESON_L_ARGS]
 EOF
 
-    # Проверка созданного файла в логах (для отладки)
-    echo "--- CROSS FILE START ---"
-    cat cross_file.txt
-    echo "--- CROSS FILE END ---"
-
-    export CPATH="$FFBUILD_PREFIX/include"
-    export LIBRARY_PATH="$FFBUILD_PREFIX/lib"
     export PKG_CONFIG_LIBDIR="$FFBUILD_PREFIX/lib/pkgconfig"
-    # перед meson setup, чтобы он увидел pcre2
     export PKG_CONFIG_PATH="$FFBUILD_PREFIX/lib/pkgconfig"
 
-#         --wrap-mode nodownload
-    # Используем -Dwrap_mode=nofallback чтобы не скачивал ничего лишнего
     meson setup build \
         --prefix="$FFBUILD_PREFIX" \
         --cross-file cross_file.txt \
         --buildtype release \
         --default-library static \
-        -Diconv=external \
+        --wrap-mode nodownload \
         -Dtests=false \
         -Dintrospection=disabled \
         -Dlibmount=disabled \
@@ -99,19 +83,18 @@ EOF
 
     # Очистка префикса (удаляем импортные библиотеки DLL, если они создались)
     find "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib" -name "*.dll.a" -delete
-    find "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib" -name "*.a.p" -rmdir 2>/dev/null || true
 }
 
 ffbuild_configure() {
     # Для FFmpeg важно знать, что glib статическая
-    echo "--enable-libglib"
+    echo "--enable-libglib";
 }
 
 ffbuild_cflags() {
-    echo "-DGLIB_STATIC_COMPILATION"
+    echo "-DGLIB_STATIC_COMPILATION";
 }
 
 ffbuild_ldflags() {
     # Glib требует системные библиотеки Windows при линковке
-    echo "-lws2_32 -lole32 -lshlwapi -luserenv -lsetupapi -liphlpapi -lintl -liconv -lpthread"
+    echo "-lws2_32 -lole32 -lshlwapi -luserenv -lsetupapi -liphlpapi -lintl -liconv -lpthread";
 }
