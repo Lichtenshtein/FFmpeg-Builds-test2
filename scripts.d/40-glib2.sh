747f99f2a1f5a6ddec5b962e0d7380a072652967
Lichtenshtein <altruumann@protonmail.com>
2026-02-12 21:52:00 +0300

    fix glib2 script

--- scripts.d/40-glib2.sh
+++ scripts.d/40-glib2.sh
@@ -9,7 +9,7 @@ ffbuild_enabled() {
 
 ffbuild_dockerdl() {
     # Изменить 'v1' на 'v2', чтобы сбросить кэш загрузки
-    echo "git-mini-clone \"$SCRIPT_REPO\" \"$SCRIPT_COMMIT\" . && echo 'v13-meson-upgrade'"
+    echo "git-mini-clone \"$SCRIPT_REPO\" \"$SCRIPT_COMMIT\" . && echo 'v14-meson-upgrade'"
 }
 
 ffbuild_dockerbuild() {
