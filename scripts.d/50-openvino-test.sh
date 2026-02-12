5e738a38424343f5c933b1b979959af3287bd393
Lichtenshtein <altruumann@protonmail.com>
2026-02-13 02:41:30 +0300

    testing openvino

--- scripts.d/50-openvino-test.sh
+++ scripts.d/50-openvino-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Прямая ссылка на архив Runtime 2024.6.0
-SCRIPT_REPO="https://storage.openvinotoolkit.org/repositories/openvino/packages/2024.6/windows/w_openvino_toolkit_windows_2024.6.0.17404.4c0f47d2335_x86_64.zip"
+# SCRIPT_REPO="https://storage.openvinotoolkit.org/repositories/openvino/packages/2024.6/windows/w_openvino_toolkit_windows_2024.6.0.17404.4c0f47d2335_x86_64.zip"
+SCRIPT_REPO="https://storage.openvinotoolkit.org/repositories/openvino/packages/2025.4.1/windows/openvino_toolkit_windows_2025.4.1.20426.82bbf0292c5_x86_64.zip"
 
 ffbuild_enabled() {
     (( $(ffbuild_ffver) >= 404 )) || return -1
@@ -17,11 +18,13 @@ ffbuild_dockerbuild() {
     cd openvino_src
 
     # Инсталляция заголовков и библиотек
-    mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX"/{include,lib,bin,cmake}
+    mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX"/{include,lib,bin}
     cp -r runtime/include/* "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/"
     cp runtime/lib/intel64/Release/*.lib "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/"
     cp runtime/bin/intel64/Release/*.dll "$FFBUILD_DESTDIR$FFBUILD_PREFIX/bin/"
     cp runtime/3rdparty/tbb/bin/*.dll "$FFBUILD_DESTDIR$FFBUILD_PREFIX/bin/"
+    # Создаем именно lib/cmake и копируем туда
+    mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/cmake"
     cp -r runtime/cmake/* "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/cmake/"
 
     mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig"
