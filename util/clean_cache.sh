0106ed1c8a99382c0cf9be99713268b7f93f03b4
Lichtenshtein <altruumann@protonmail.com>
2026-02-15 02:39:15 +0300

    get rid of wget's freaky 11k lines dot wall progress bar

--- util/clean_cache.sh
+++ util/clean_cache.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-set -xe
-cd "$(dirname "$0")"/../.cache/downloads
-find . $(printf "! -name %s " $(find . -type l -exec basename -a {} + -exec readlink {} +)) -delete
+
+# set -xe
+# cd "$(dirname "$0")"/../.cache/downloads
+# find . $(printf "! -name %s " $(find . -type l -exec basename -a {} + -exec readlink {} +)) -delete
+
+set -e
+cd .cache/downloads || exit 0
+# Удаляем только те файлы .tar.xz, на которые НЕТ симлинков в этой же папке
+find . -name "*.tar.xz" -type f -not -name "*_*" -links 1 -delete
