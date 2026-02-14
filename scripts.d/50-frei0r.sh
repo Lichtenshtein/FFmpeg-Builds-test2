098da321e3ad3e8142796119528cbd3fc77ee1f5
Lichtenshtein <altruumann@protonmail.com>
2026-02-14 14:26:19 +0300

    fix 50-frei0r

--- scripts.d/50-frei0r.sh
+++ scripts.d/50-frei0r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/dyne/frei0r.git"
-SCRIPT_COMMIT="6d272815f25baa6daafacdd132a43e36a94ff21c"
+SCRIPT_COMMIT="ced05b4fcb94481d9b8fb81b4af3e63bd8026491"
 
 ffbuild_enabled() {
     [[ $VARIANT == lgpl* ]] && return -1
@@ -21,13 +21,23 @@ ffbuild_dockerfinal() {
 ffbuild_dockerbuild() {
     mkdir build && cd build
 
+    # флаги для игнорирования несовместимых типов в SIMD коде (актуально для GCC 14)
+    # Флаг -flax-vector-conversions разрешает неявное приведение __m128i к __m128
+    export CFLAGS="$CFLAGS -flax-vector-conversions -Wno-error=incompatible-pointer-types"
+    export CXXFLAGS="$CXXFLAGS -flax-vector-conversions -Wno-error=incompatible-pointer-types"
+
+        # -DWITHOUT_OPENCV=ON
     cmake -G Ninja \
         -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_C_FLAGS="$CFLAGS" \
         -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
         -DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS" \
-        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" ..
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        -DWITHOUT_GAVL=ON \
+        ..
+
     ninja -j$(nproc) $NINJA_V
     DESTDIR="$FFBUILD_DESTDIR" ninja install
 }
