5e112598844d8373f8e7d11f31457696a9d71d71
Lichtenshtein <altruumann@protonmail.com>
2026-02-21 21:04:02 +0300

    fix glib2

--- scripts.d/25-glib2.sh
+++ scripts.d/25-glib2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/GNOME/glib.git"
-# SCRIPT_COMMIT="05da47c2d0d333cec6363e09bcf9eed6952e74ce"
-SCRIPT_COMMIT="6b11cae1b3bf3e9cff9485481dd1c0f7e806c361"
+SCRIPT_COMMIT="2371bee17d85318480b3ddeeab4f5107b4889ad7"
+# SCRIPT_COMMIT="6b11cae1b3bf3e9cff9485481dd1c0f7e806c361"
 
 ffbuild_enabled() {
     return 0
@@ -45,21 +45,22 @@ ranlib = '${FFBUILD_TOOLCHAIN}-gcc-ranlib'
 [properties]
 posix_memalign_with_alignment = false
 growstack = false
-# have_c99_snprintf = true
-# have_c99_vsnprintf = true
+have_c99_snprintf = true
+have_c99_vsnprintf = true
 va_val_copy = true
 growing_stack = false
-needs_exe_wrapper = true
+# needs_exe_wrapper = true
+needs_exe_wrapper = false
 # has_function_gettext = true
 # has_function_ngettext = true
 # has_function_bindtextdomain = true
 # exe_wrapper = '/usr/libexec/wine'
-exe_wrapper = '/usr/lib/x86_64-linux-gnu/wine'
+# exe_wrapper = '/usr/lib/x86_64-linux-gnu/wine'
 # /usr/lib/wine /usr/include/wine
 
 [built-in options]
-c_args = ['-I${FFBUILD_PREFIX}/include']
-cpp_args = []
+c_args = ['-I${FFBUILD_PREFIX}/include', '-DGLIB_STATIC_COMPILATION', '-D_WIN32_WINNT=0x0A00']
+cpp_args = ['-I${FFBUILD_PREFIX}/include', '-DGLIB_STATIC_COMPILATION', '-D_WIN32_WINNT=0x0A00']
 c_link_args = ['-L${FFBUILD_PREFIX}/lib', '-lintl', '-liconv']
 cpp_link_args = ['-L${FFBUILD_PREFIX}/lib', '-lintl', '-liconv']
 EOF
@@ -67,11 +68,14 @@ EOF
     # Настройка окружения для Meson
     export PKG_CONFIG_LIBDIR="$FFBUILD_PREFIX/lib/pkgconfig:$FFBUILD_PREFIX/share/pkgconfig"
     unset PKG_CONFIG_PATH 
-    export CFLAGS="$CFLAGS -D_G_WIN32_WINNT=0x0601 -DG_WINAPI_ONLY_APP=false -DGLIB_STATIC_COMPILATION"
-    export CXXFLAGS="$CXXFLAGS -D_G_WIN32_WINNT=0x0601 -DG_WINAPI_ONLY_APP=false -DGLIB_STATIC_COMPILATION"
 
     # Удаляем субпроекты, которые ломают сборку
     rm -rf subprojects/sysprof subprojects/pcre2 subprojects/libffi
+
+    # Ключевые флаги: 
+    # 1. -D_WIN32_WINNT=0x0A00 (Win10)
+    # 2. b_vscrt=static_fallback (важно для MinGW)
+    # 3. x_uwp=false (ГЛАВНОЕ: отключает UWP принудительно)
 
     meson setup build \
         --prefix="$FFBUILD_PREFIX" \
@@ -79,14 +83,20 @@ EOF
         --buildtype release \
         --default-library static \
         --wrap-mode=nodownload \
-        -Dnls=disabled \
         -Dtests=false \
-        -Db_coverage=false \
-        -Dman-pages=disabled \
+        -Db_vscrt=static_fallback \
+        -Dx_uwp=false \
         -Dintrospection=disabled \
         -Dlibmount=disabled \
+        -Dnls=enabled \
         -Dglib_debug=disabled \
-        -Dgio_module_dir="$FFBUILD_PREFIX/lib/gio/modules"
+        -Dglib_asserts=false \
+        -Dglib_checks=false \
+        -Druntime_libdir="" \
+        -Dman-pages=disabled \
+        -Dselinux=disabled \
+        -Dsysprof=disabled \
+        || (cat build/meson-logs/meson-log.txt && exit 1)
 
     ninja -C build -j$(nproc) $NINJA_V
     DESTDIR="$FFBUILD_DESTDIR" ninja -C build install
@@ -97,7 +107,7 @@ EOF
     local PC_FILE="$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig/glib-2.0.pc"
     if [[ -f "$PC_FILE" ]]; then
         # Добавляем зависимости, которые Meson часто забывает для static win64
-        sed -i 's/^Libs:/Libs: -lws2_32 -lole32 -lshlwapi -luserenv -lsetupapi -liphlpapi /' "$PC_FILE"
+        sed -i 's/^Libs:/Libs: -lws2_32 -lole32 -lshlwapi -luserenv -lsetupapi -liphlpapi -lwinmm -ldnsapi/' "$PC_FILE"
     fi
 }
 
@@ -111,5 +121,5 @@ ffbuild_cflags() {
 }
 
 ffbuild_libs() {
-    echo "-luserenv -liphlpapi -lintl -liconv -lpthread"
+    echo "-luserenv -liphlpapi -lintl -liconv -lwinmm -ldnsapi -lpthread"
 }
