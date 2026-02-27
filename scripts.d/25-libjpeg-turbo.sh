7d7bc383ee74d77be52a59bc869997ec78d9c3f9
Lichtenshtein <altruumann@protonmail.com>
2026-02-27 11:59:28 +0300

    fix libtiff

--- scripts.d/25-libjpeg-turbo.sh
+++ scripts.d/25-libjpeg-turbo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/libjpeg-turbo/libjpeg-turbo.git"
-SCRIPT_COMMIT="af9c1c268520a29adf98cad5138dafe612b3d318"
+SCRIPT_COMMIT="4d293d9400281045e062b6e4eb8e1ccfc89d91f8"
 
 ffbuild_enabled() {
     return 0
@@ -14,18 +14,21 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     mkdir build && cd build
 
-    local myconf=(
-        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN"
-        -DCMAKE_BUILD_TYPE=Release
-        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX"
-        -DENABLE_SHARED=OFF
-        -DENABLE_STATIC=ON
-        -DWITH_JPEG8=ON
-        -DWITH_CRT_DLL=OFF
-    )
-
     # На Broadwell libjpeg-turbo будет использовать AVX2 автоматически через NASM
-    cmake "${myconf[@]}" -DCMAKE_C_FLAGS="$CFLAGS" ..
+    cmake -G "Unix Makefiles" \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        -DENABLE_SHARED=OFF \
+        -DENABLE_STATIC=ON \
+        -DWITH_JPEG8=ON \
+        -DWITH_SIMD=ON \
+        -DWITH_TOOLS=OFF \
+        -DWITH_TESTS=OFF \
+        -DWITH_TURBOJPEG=ON \
+        -DWITH_CRT_DLL=OFF \
+        -DCMAKE_C_FLAGS="$CFLAGS" ..
 
     make -j$(nproc) $MAKE_V
     make install DESTDIR="$FFBUILD_DESTDIR"
