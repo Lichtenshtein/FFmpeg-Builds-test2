8217fc92ff4574b99db3a3357fcf77f6cbff8998
Lichtenshtein <altruumann@protonmail.com>
2026-02-27 14:40:25 +0300

    rollback to 1.6 branch for libpng

--- scripts.d/25-libpng.sh
+++ scripts.d/25-libpng.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/glennrp/libpng.git"
-SCRIPT_COMMIT="0094fdbf3743c238effb88aa92cf2a2ea23ade4a"
+SCRIPT_COMMIT="c3e304954a9cfd154bc0dfbfea2b01cd61d6546d"
 
 ffbuild_depends() {
     echo base
@@ -29,11 +29,22 @@ ffbuild_dockerbuild() {
         --with-pic
     )
 
+    # Добавляем LTO если включено
+    if [[ "$USE_LTO" == "1" ]]; then
+        export CFLAGS="$CFLAGS -flto"
+        export LDFLAGS="$LDFLAGS -flto"
+    fi
+
+    # Гарантируем, что zlib будет найден
     export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include"
+    export LDFLAGS="$LDFLAGS -L$FFBUILD_PREFIX/lib"
 
     ./configure "${myconf[@]}"
     make -j$(nproc) $MAKE_V
     make install DESTDIR="$FFBUILD_DESTDIR"
 
-    ln -sf libpng.pc "$FFBUILD_DESTPREFIX"/lib/pkgconfig/libpng16.pc
+    # Создаем стандартные симлинки для совместимости
+    # Многие старые пакеты ищут libpng16.pc или libpng.pc
+    cd "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig"
+    ln -sf libpng16.pc libpng.pc
 }
