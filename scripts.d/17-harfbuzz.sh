e147245b2e36f47bcc76e3cfd4c5f2f889388e45
Lichtenshtein <altruumann@protonmail.com>
2026-03-24 21:05:55 +0300

    rename back libicu

--- scripts.d/17-harfbuzz.sh
+++ scripts.d/17-harfbuzz.sh
@@ -20,7 +20,7 @@ ffbuild_dockerbuild() {
     set -e
     mkdir build && cd build
 
-    local DEP_LIBS="-lfreetype -licuin -licuuc -licudt"
+    local DEP_LIBS="-lfreetype -lsicuin -lsicuuc -lsicudt"
     local WIN_LIBS="-lusp10 -lgdi32 -lrpcrt4 $LIBS"
 
     local myconf=(
