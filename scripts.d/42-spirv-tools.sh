0387e5163d235ddf1565745d480e447c42399a32
Lichtenshtein <altruumann@protonmail.com>
2026-03-30 23:05:26 +0300

    add patch for static library

--- scripts.d/42-spirv-tools.sh
+++ scripts.d/42-spirv-tools.sh
@@ -14,21 +14,6 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     set -e
 
-    # Заменяем упоминание shared.pc на обычный .pc во всех DEPENDS и установках
-    # Это сохранит синтаксис скобок, но заставит CMake делать одно и то же дважды
-    sed -i 's/SPIRV-Tools-shared.pc/SPIRV-Tools.pc/g' CMakeLists.txt
-
-    # Ослепляем установку целей shared в source/CMakeLists.txt
-    # Просто комментируем строки, где есть упоминание shared библиотек
-    if [ -f "source/CMakeLists.txt" ]; then
-        sed -i 's/.*-shared.*/# \0/g' source/CMakeLists.txt
-    fi
-
-    # Убираем экспорт shared целей из основного конфига (чтобы не ломать glslang)
-    # Заменяем имя цели на статическую, чтобы не было ошибки "target not found"
-    find . -name "*.cmake" -exec sed -i 's/SPIRV-Tools-shared/SPIRV-Tools/g' {} +
-    find . -name "CMakeLists.txt" -exec sed -i 's/SPIRV-Tools-shared/SPIRV-Tools/g' {} +
-
     mkdir build && cd build
 
     local myconf=(
@@ -36,6 +21,8 @@ ffbuild_dockerbuild() {
         -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX"
         -DCMAKE_BUILD_TYPE=Release
         -DBUILD_SHARED_LIBS=OFF
+        -DSPIRV_TOOLS_BUILD_SHARED=OFF
+        -DSPIRV_TOOLS_BUILD_STATIC=ON
         -DSKIP_SPIRV_TOOLS_INSTALL=OFF
         -DSPIRV_CHECK_CONTEXT=OFF
         -DSPIRV_SKIP_EXECUTABLES=ON
