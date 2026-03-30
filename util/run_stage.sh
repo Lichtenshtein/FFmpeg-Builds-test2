dfe246786c27e32d4960df97f19541b4b83e8cb6
Lichtenshtein <altruumann@protonmail.com>
2026-03-30 19:56:23 +0300

    remove traces of shared library for SPIRV-Tools

--- util/run_stage.sh
+++ util/run_stage.sh
@@ -246,11 +246,10 @@ fi
 PRESERVE_DLL_PATTERN="${DLL_PRESERVE_LIST// /:-openvino|torch|tensorflow|vulkan|amf|nvcodec|mfx|onevpl}"
 if [[ ! "$STAGENAME" =~ $PRESERVE_DLL_PATTERN ]]; then
     if [[ -d "$FFBUILD_DESTDIR$FFBUILD_PREFIX" ]]; then
-        DELETED_FILES=$(find "$FFBUILD_DESTDIR$FFBUILD_PREFIX" -type f \( -name "*.dll" -o -name "*.dll.a" \) -print)
+        DELETED_FILES=$(find "$FFBUILD_DESTDIR$FFBUILD_PREFIX" -type f \( -name "*.dll" -o -name "*.dll.a" \) -print | sed "s|$FFBUILD_DESTDIR||g")
         if [[ -n "$DELETED_FILES" ]]; then
             log_info "################################################################"
-            log_debug "${BROOM_MARK} Cleaning unwanted dynamic DLLs from static stage: $STAGENAME"
-            log_info "$DELETED_FILES" | sed "s|$FFBUILD_DESTDIR||g"
+            log_debug "${BROOM_MARK} Cleaning unwanted dynamic DLLs from static stage: $STAGENAME\n$DELETED_FILES" 
             # find "$FFBUILD_DESTDIR$FFBUILD_PREFIX" -type f -name "*.dll" -delete || true
             find "$FFBUILD_DESTDIR$FFBUILD_PREFIX" -type f \( -name "*.dll" -o -name "*.dll.a" \) -delete || true
         fi
