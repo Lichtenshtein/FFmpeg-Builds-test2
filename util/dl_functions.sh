00b8efe268a451cc3a07a810cc3b7a9c91e557f8
Lichtenshtein <altruumann@protonmail.com>
2026-04-02 23:03:14 +0300

    test

--- util/dl_functions.sh
+++ util/dl_functions.sh
@@ -399,6 +399,7 @@ download_stage() {
         # source "$UTIL_DIR/vars.sh" "$TARGET" "$VARIANT" &>/dev/null
         # source "$UTIL_DIR/dl_functions.sh"
         eval "$DL_COMMANDS"
+        trap 'rm -rf "$WORK_DIR"' EXIT
     ); then
 
         # Whitelist метаданных .git (список подгружается из workflow.yaml). 
