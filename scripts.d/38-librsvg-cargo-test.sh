0998147c1364f8945fdaa7ceaf9a2a3ed427f9d8
Lichtenshtein <altruumann@protonmail.com>
2026-04-05 15:03:12 +0300

    add nettle; fix scripts

--- scripts.d/38-librsvg-cargo-test.sh
+++ scripts.d/38-librsvg-cargo-test.sh
@@ -25,20 +25,26 @@ ffbuild_dockerdl() {
 
 ffbuild_dockerbuild() {
     set -e
+
+    local DEP_LIBS="-lpangocairo-1.0 -lpangowin32-1.0 -lpangoft2-1.0 -lpango-1.0 -lgio-2.0 -lgthread-2.0 -lglib-2.0 -ldav1d -lxml2 -lintl -liconv -lcharset"
+    local WIN_LIBS="-luserenv -lusp10 -lruntimeobject -lntdll -lmsimg32 -lgdi32 -lstdc++"
+
     # Настройки для Cargo (Rust кросс-компиляция)
     export CARGO_HOME="/opt/cargo"
     export RUSTUP_HOME="/opt/rustup"
-    # Ключевой момент: указываем Cargo использовать наш Mingw-линковщик
+    # Указываем Cargo использовать наш Mingw-линковщик
     # Имя переменной должно строго соответствовать таргету в верхнем регистре
     export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER="${FFBUILD_TOOLCHAIN}-gcc"
     export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_AR="${FFBUILD_TOOLCHAIN}-gcc-ar"
     export PKG_CONFIG_ALLOW_CROSS=1
 
-    # Помогаем Rust найти либы C через переменные окружения
-    export RUSTFLAGS="-L native=$FFBUILD_PREFIX/lib -C linker=${FFBUILD_TOOLCHAIN}-gcc"
-
+    CFLAGS="$CFLAGS $CPPFLAGS" \
+    CXXFLAGS="$CXXFLAGS $CPPFLAGS" \
+    LDFLAGS="$LDFLAGS" \
+    LIBS="$DEP_LIBS $WIN_LIBS $LIBS"
+    RUSTFLAGS="${RUSTFLAGS} -L native=$FFBUILD_PREFIX/lib -C linker=${FFBUILD_TOOLCHAIN}-gcc" \
     cargo cinstall --release \
-        --target="x86_64-pc-windows-gnu" \
+        --target="${FFBUILD_RUST_TARGET}" \
         --prefix="$FFBUILD_PREFIX" \
         --destdir="$FFBUILD_DESTDIR" \
         --library-type=staticlib \
@@ -49,10 +55,12 @@ ffbuild_dockerbuild() {
     # librsvg-2.0.pc после сборки часто не содержит Cairo/Pango в Requires.private
     local PC_FILE="$PC_DIR/librsvg-2.0.pc"
     if [[ -f "$PC_FILE" ]]; then
-        # Librsvg (Rust) генерирует огромную статическую либу, которой нужны ВСЕ системные либы Windows
-        sed -i 's/^Libs:.*/& -lpangocairo-1.0 -lpangowin32-1.0 -lpangoft2-1.0 -lpango-1.0 -lgio-2.0 -lgthread-2.0 -lglib-2.0 -ldav1d -lxml2 -lintl -liconv -lcharset -luserenv -lusp10 -lruntimeobject -lntdll -lmsimg32 -lgdi32 -lstdc++/' "$PC_FILE"
+        if grep -q "^Libs.private:" "$PC_FILE"; then
+            sed -i "s|^Libs.private:.*|Libs.private: $DEP_LIBS $WIN_LIBS|" "$PC_FILE"
+        else
+            sed -i "/^Libs:/ a Libs.private: $DEP_LIBS $WIN_LIBS" "$PC_FILE"
+        fi
     fi
-
 }
 
 ffbuild_configure() {
