13fbd20183827857b6465879d95530a127ff2aa9
Lichtenshtein <altruumann@protonmail.com>
2026-04-12 22:04:19 +0300

    ffi keeps failing

--- variants/win64-gpl-shared.sh
+++ variants/win64-gpl-shared.sh
@@ -3,5 +3,5 @@ source "$(dirname "${BASH_SOURCE}")"/windows-install-shared.sh
 source "$(dirname "${BASH_SOURCE}")"/defaults-gpl.sh
 
 ffbuild_configure() {
-    echo "--pkg-config-flags='' --enable-shared --disable-static --enable-gpl --enable-version3"
+    echo "--pkg-config-flags=\"\" --enable-shared --disable-static --enable-gpl --enable-version3"
 }
