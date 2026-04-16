ed0c84e1f81d1903643b274a6fd8dbe6f79d4f78
Lichtenshtein <altruumann@protonmail.com>
2026-04-16 09:10:50 +0300

    test aom

--- scripts.d/70-aom.sh
+++ scripts.d/70-aom.sh
@@ -26,6 +26,10 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     set -e
 
+    sed -i 's/add_library(aom_hwy/add_library(aom_hwy STATIC/g' CMakeLists.txt
+    # Если есть файл с hwy, принудительно ставим ему язык CXX
+    echo "set_target_properties(aom_hwy PROPERTIES LINKER_LANGUAGE CXX)" >> CMakeLists.txt
+
     mkdir -p _build && cd _build
 
     local myconf=(
@@ -35,6 +39,7 @@ ffbuild_dockerbuild() {
         -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX"
         -DBUILD_SHARED_LIBS=$([ "${PREFER_SHARED}" == "1" ] && echo ON || echo OFF)
         -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+        -DCMAKE_POLICY_DEFAULT_CMP0169=OLD
         -DCMAKE_POLICY_DEFAULT_CMP0135=NEW
         -DENABLE_EXAMPLES=OFF
         -DENABLE_TESTS=OFF
@@ -62,6 +67,9 @@ ffbuild_dockerbuild() {
         -DCONFIG_AV1_HIGHBITDEPTH=1
         # можно выключить и не тянуть лишний код контейнеров внутрь библиотеки
         -DCONFIG_WEBM_IO=1
+        # Попытка вылечить "can not determine linker language"
+        -DCMAKE_CXX_STANDARD=17 
+        -DCMAKE_CXX_STANDARD_REQUIRED=ON
     )
 
     CFLAGS="$CFLAGS $CPPFLAGS -Wno-dev" \
