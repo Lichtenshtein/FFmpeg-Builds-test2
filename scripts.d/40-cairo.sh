75a4da7a7a2fa0dbb4985d3e7b1611b60cc2f5dd
Lichtenshtein <altruumann@protonmail.com>
2026-04-16 22:47:33 +0300

    test frei0r

--- scripts.d/40-cairo.sh
+++ scripts.d/40-cairo.sh
@@ -30,7 +30,7 @@ ffbuild_dockerbuild() {
     # Зависимости из чит-листа в правильном порядке линковки
     local DEP_LIBS="-lfontconfig -lpixman-1 -lxml2 -lpng16 -lgio-2.0 -lgthread-2.0 -lglib-2.0 -lfreetype -lbz2 -lbrotlienc -lbrotlidec -lbrotlicommon -lz -lintl -liconv -lcharset -lsicuin -lsicuuc -lsicudt"
     # Набор системных библиотек Windows для Cairo
-    local WIN_LIBS="-lgdi32 -lmsimg32 -ldwrite -ld2d1 -lwindowscodecs -lopengl32 -luuid $LIBS"
+    local WIN_LIBS="-lgdi32 -lmsimg32 -ldwrite -ld2d1 -lwindowscodecs -lopengl32 -luuid $LIBS -lstdc++"
 
     # конфликт hypot в коде Cairo для MinGW
     # error: implicit declaration of function '_hypot'
