4c77db34d492070672be5cb0a1648a49b63263d2
Lichtenshtein <altruumann@protonmail.com>
2026-04-17 19:36:40 +0300

    update external libraries scripts

--- scripts.d/59-libtorch-test.sh
+++ scripts.d/59-libtorch-test.sh
@@ -13,24 +13,26 @@ ffbuild_dockerdl() {
 
 ffbuild_dockerbuild() {
     set -e
+
     # Распаковываем (unzip должен быть в base образе)
     unzip -qq libtorch.zip
+
     # Находим папку (имя может меняться в зависимости от билда)
     local LT_DIR=$(find . -maxdepth 1 -type d -name "libtorch*" | head -n 1)
     cd "$LT_DIR"
 
     # cd libtorch_src
 
-    mkdir -p "$FFBUILD_DESTPREFIX"/{include,lib,bin}
-    
-    # Копируем всё содержимое
-    cp -r include/* "$FFBUILD_DESTPREFIX/include/"
+    mkdir -p "$INSTALL_ROOT"/{include,lib,bin}
+
     # Копируем заголовочные файлы
-    # Для MinGW важно, чтобы .lib файлы имели префикс lib, иначе -l не всегда их видит
-    for f in lib/*.lib; do 
-        cp "$f" "$FFBUILD_DESTPREFIX/lib/lib$(basename "$f" .lib).dll.a"
-    done
-    cp lib/*.dll "$FFBUILD_DESTPREFIX/bin/"
+    cp -r include/* "$INSTALL_ROOT/include/"
+
+    # Копируем библиотеки и DLL
+    # копируем как есть. Общая функция сама решит, 
+    # сделать ли из .lib -> .a или сгенерировать импорт из .dll
+    cp lib/*.lib "$INSTALL_ROOT/lib/" 2>/dev/null || true
+    cp lib/*.dll "$INSTALL_ROOT/bin/" 2>/dev/null || true
 
     # LibTorch требует много флагов, создаем .pc файл
     mkdir -p "$PC_DIR"
@@ -42,15 +44,16 @@ includedir=\${prefix}/include
 Name: LibTorch
 Description: PyTorch C++ API
 Version: 2.10.0
-# Переносим основные либы в Libs, чтобы они всегда были видны
 Libs: -L\${libdir} -ltorch -ltorch_cpu -lc10
-# Добавляем необходимые системные либы Windows в private
-# Все зависимости, которые нужны только при статической линковке самого FFmpeg к этим DLL
 Libs.private: -lshlwapi -luser32 -ladvapi32 -lstdc++
 Cflags: -I\${includedir} -I\${includedir}/torch/csrc/api/include -D_GLIBCXX_USE_CXX11_ABI=1 -DNOMINMAX
 EOF
-
 }
 
-ffbuild_configure() { echo --enable-libtorch; }
-ffbuild_unconfigure() { echo --disable-libtorch; }
+ffbuild_configure() {
+    echo --enable-libtorch
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libtorch
+}
