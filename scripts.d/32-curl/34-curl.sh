6e352d39d9098a820e78a6f451b3125e704a655b
Lichtenshtein <altruumann@protonmail.com>
2026-04-20 05:30:41 +0300

    test curl

--- scripts.d/32-curl/34-curl.sh
+++ scripts.d/32-curl/34-curl.sh
@@ -24,21 +24,18 @@ ffbuild_dockerbuild() {
 
     autoreconf -fi
 
-    # Это заставит curl увидеть все зависимости quiche из Libs.private во время тестов
+    sed -i 's/include <sys\/socket.h>/include <winsock2.h>/g' configure
+
     sed -i 's/\$PKGCONFIG --libs-only-l quiche/\$PKGCONFIG --libs-only-l --static quiche/g' configure
     sed -i 's/\$PKGCONFIG --libs-only-l libssh/\$PKGCONFIG --libs-only-l --static libssh/g' configure
 
     export PKG_CONFIG_PATH="$FFBUILD_PREFIX/lib/pkgconfig:$FFBUILD_PREFIX/share/pkgconfig"
-    export PKG_CONFIG_LIBDIR="$PKG_CONFIG_PATH"
     export PKG_CONFIG_ALLOW_CROSS=1
 
     # Выделяем из CFLAGS только флаги компилятора (без -D и -I)
     local CLEAN_CFLAGS=$(echo "$CFLAGS" | sed 's/-[DU][^ ]*//g; s/-I[^ ]*//g')
 
-    # Собираем системные либы для Windows (OpenSSL требует bcrypt и advapi32)
-    # Порядок: curl -> (+crypto, quiche) -> ssh -> openssl -> [zstd, brotli, zlib] -> [системные]
-    local DEP_LIBS="-lssh -lquiche -lssl -lcrypto -lnghttp2 -lzstd -lbrotlidec -lbrotlicommon -lz"
-    local WIN_SYS_LIBS="-lws2_32 -lbcrypt -lcrypt32 -liphlpapi -lntdll -ladvapi32 -luser32 -lshlwapi -lole32 -lsetupapi -lgomp -lpthread -lm"
+    export LIBS="-lws2_32 -lbcrypt -lpthread -lm"
 
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
@@ -93,10 +90,9 @@ ffbuild_dockerbuild() {
     CPPFLAGS="$CPPFLAGS $self_static_flags $static_flags" \
     CXXFLAGS="$CXXFLAGS $self_static_flags $static_flags ${USELTO}" \
     LDFLAGS="$LDFLAGS ${USELTO}" \
-    LIBS="$DEP_LIBS $WIN_SYS_LIBS $LIBS" \
     ./configure "${myconf[@]}" || {
-        echo "❌ FAILED. Look at the end of config.log for link errors:"
-        tail -n 100 config.log
+        log_error "FAILED. Look at the end of config.log for link errors:"
+        grep -A 20 "checking for quiche_conn_send_ack_eliciting" config.log | grep -v "lt_cv"
         return 1
     }
 
