aa5cd89ce8b29edebf05b23ab737f65aadc4e563
Lichtenshtein <altruumann@protonmail.com>
2026-04-21 06:45:14 +0300

    fix fridibi produces shared libs

--- scripts.d/39-fribidi.sh
+++ scripts.d/39-fribidi.sh
@@ -21,7 +21,7 @@ ffbuild_dockerbuild() {
     local myconf=(
         --buildtype=release
         --cross-file="$FFBUILD_MESON_CROSS"
-        --default-library=$([ "${PREFER_SHARED}" == "1" ] && echo static || echo shared)
+        --default-library=$([ "${PREFER_SHARED}" == "1" ] && echo shared || static)
         --prefix="$FFBUILD_PREFIX"
         -Db_lto=$([ "${USE_LTO}" == "1" ] && echo true || echo false )
         -Dbin=false
