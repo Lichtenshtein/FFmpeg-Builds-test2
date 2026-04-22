694a1d948ff862de9d17bd56d14e7564cb8e5ddc
Lichtenshtein <altruumann@protonmail.com>
2026-04-22 07:52:15 +0300

    test libklvanc

--- scripts.d/48-quirc.sh
+++ scripts.d/48-quirc.sh
@@ -20,6 +20,20 @@ ffbuild_dockerbuild() {
     mkdir -p "$FFBUILD_DESTPREFIX/lib/" "$FFBUILD_DESTPREFIX/include/"
     cp libquirc.a "$FFBUILD_DESTPREFIX/lib/"
     cp lib/quirc.h "$FFBUILD_DESTPREFIX/include/"
+
+    mkdir -p "$PC_DIR"
+    cat <<EOF > "$PC_DIR/quirc.pc"
+prefix=$FFBUILD_PREFIX
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+includedir=\${prefix}/include
+
+Name: quirc
+Description: QR decoder library
+Version: 1.2
+Libs: -L\${libdir} -lquirc
+Cflags: -I\${includedir}
+EOF
 }
 
 ffbuild_configure() {
