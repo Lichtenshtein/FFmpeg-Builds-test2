2855cbb9e04ca868b756c1d3f8add047d231d3e9
Lichtenshtein <altruumann@protonmail.com>
2026-04-23 03:34:44 +0300

    test libmpg123

--- scripts.d/66-libmpg123.sh
+++ scripts.d/66-libmpg123.sh
@@ -3,7 +3,7 @@
 SCRIPT_REPO="https://github.com/madebr/mpg123.git"
 SCRIPT_COMMIT="64972c017377bf8972d8d245bff8234a2032a3d2"
 
-export USE_CONF_FINDER=1
+# export USE_CONF_FINDER=1
 
 ffbuild_enabled() {
     return 0
@@ -17,7 +17,7 @@ ffbuild_dockerbuild() {
     set -e
 
     # autoreconf -iv
-    # autoreconf -fiv
+    autoreconf -fiv
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
         --host="$FFBUILD_TOOLCHAIN"
