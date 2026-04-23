9132d9139e259b7393d3a6d748c149cd5decfe84
Lichtenshtein <altruumann@protonmail.com>
2026-04-23 08:05:44 +0300

    test libopus

--- scripts.d/67-libopus.sh
+++ scripts.d/67-libopus.sh
@@ -10,20 +10,22 @@ ffbuild_enabled() {
 ffbuild_dockerdl() {
     default_dl .
     # This is where they decided to put downloads for external dependencies
-    # This shit highly polutes the download log
-    # echo "./autogen.sh"
+    # This highly pollutes the download log, try to make it silent
+    cat <<EOF
+sed -i -e 's/wget /wget -q /g' -e 's/tar xvzomf/tar xzomf/g' /dnn/download_model.sh
+EOF
+    echo "./autogen.sh"
 
     # https://github.com/xiph/opus/blob/main/autogen.sh
-    # Ссылка и хэш для проверки
-    local OPUS_DATA_URL="https://media.xiph.org/opus/models/opus_data-a5177ec6fb7d15058e99e57029746100121f68e4890b1467d4094aa336b6013e.tar.gz"
-    local OPUS_DATA_HASH="a5177ec6fb7d15058e99e57029746100121f68e4890b1467d4094aa336b6013e"
-    cat <<EOF
-mkdir -p dnn
-curl -sL --retry 10 --retry-delay 5 --connect-timeout 30 "$OPUS_DATA_URL" -o "dnn/opus_data-${OPUS_DATA_HASH}.tar.gz"
-echo "$OPUS_DATA_HASH  dnn/opus_data-${OPUS_DATA_HASH}.tar.gz" | sha256sum -c -
-tar -xvzomf "dnn/opus_data-${OPUS_DATA_HASH}.tar.gz" -C dnn
-rm -f "dnn/opus_data-${OPUS_DATA_HASH}.tar.gz"
-EOF
+    # local OPUS_DATA_URL="https://media.xiph.org/opus/models/opus_data-a5177ec6fb7d15058e99e57029746100121f68e4890b1467d4094aa336b6013e.tar.gz"
+    # local OPUS_DATA_HASH="a5177ec6fb7d15058e99e57029746100121f68e4890b1467d4094aa336b6013e"
+    # cat <<EOF
+# mkdir -p dnn
+# curl -sL --retry 10 --retry-delay 5 --connect-timeout 30 "$OPUS_DATA_URL" -o "dnn/opus_data-${OPUS_DATA_HASH}.tar.gz"
+# echo "$OPUS_DATA_HASH  dnn/opus_data-${OPUS_DATA_HASH}.tar.gz" | sha256sum -c -
+# tar -xzomf "dnn/opus_data-${OPUS_DATA_HASH}.tar.gz"
+# rm -f "dnn/opus_data-${OPUS_DATA_HASH}.tar.gz"
+# EOF
 }
 
 ffbuild_dockerbuild() {
@@ -42,11 +44,12 @@ ffbuild_dockerbuild() {
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         -DOPUS_CUSTOM_MODES=ON
         -DOPUS_FLOAT_APPROX=ON
+        -DOPUS_X86_PRESUME_AVX2=ON
         -DOPUS_INSTALL_CMAKE_CONFIG_MODULE=ON
         -DOPUS_DRED=ON # neural scan and restoring for loss packets on low bitrates
         -DOPUS_OSCE=ON # AI-based speech enhancement feature
         -DOPUS_BUILD_FRAMEWORK=OFF # bundle for Apple systems
-        -DOPUS_STATIC_RUNTIME=OFF # MSVC; build with static runtime library
+        # -DOPUS_STATIC_RUNTIME=OFF # MSVC; build with static runtime library
         -DOPUS_ENABLE_FLOAT_API=ON
         -DOPUS_HARDENING=ON
         -DOPUS_INSTALL_PKG_CONFIG_MODULE=ON
