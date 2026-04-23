b1dd2d4ad22e4cfbd38e771fa04da99fd56ebe39
Lichtenshtein <altruumann@protonmail.com>
2026-04-23 11:05:03 +0300

    test mp3shine

--- scripts.d/67-twolame.sh
+++ scripts.d/67-twolame.sh
@@ -21,7 +21,7 @@ ffbuild_dockerbuild() {
 
     # libtoolize version detection is broken, disable it, we got the right versions
     printf 'print "999999\\n"\n' > autogen-get-version-mock.pl
-    # sed -i -e 's|/autogen-get-version.pl|/autogen-get-version-mock.pl|g' ./autogen.sh
+    sed -i -e 's|/autogen-get-version.pl|/autogen-get-version-mock.pl|g' ./autogen.sh
 
     # NOCONFIGURE=1 ./autogen.sh
 
