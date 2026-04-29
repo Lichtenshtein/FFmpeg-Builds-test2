90ad63ddf1da96a1975e3e7eba9065c793d2efeb
Lichtenshtein <altruumann@protonmail.com>
2026-04-29 19:40:43 +0300

    test libtorch

--- scripts.d/59-libtorch-test.sh
+++ scripts.d/59-libtorch-test.sh
@@ -71,7 +71,7 @@ Name: LibTorch
 Description: PyTorch C++ API
 Version: 2.10.0
 Libs: -L\${libdir} -ltorch -ltorch_cpu -lc10
-Cflags: -I\${includedir} -I\${includedir}/torch/csrc/api/include -D_GLIBCXX_USE_CXX11_ABI=0 -DNOMINMAX
+Cflags: -I\${includedir} -I\${includedir}/torch/csrc/api/include -D_GLIBCXX_USE_CXX11_ABI=1 -DNOMINMAX
 EOF
 }
 
