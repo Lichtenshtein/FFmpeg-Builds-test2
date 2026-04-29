94cd82a6f64869517dfa0094a0d3c002d15626d8
Lichtenshtein <altruumann@protonmail.com>
2026-04-29 13:27:23 +0300

    test libtensorflow

--- variants/win64-gpl-shared.sh
+++ variants/win64-gpl-shared.sh
@@ -2,6 +2,9 @@
 source "$(dirname "${BASH_SOURCE}")"/windows-install-shared.sh
 source "$(dirname "${BASH_SOURCE}")"/defaults-gpl.sh
 
+eval "prev_ffbuild_configure_gpl() { $(declare -f ffbuild_configure | sed '1,2d;$d'); }"
+
 ffbuild_configure() {
+    prev_ffbuild_configure_gpl 2>/dev/null || true
     echo "--pkg-config-flags=\"\" --enable-shared --disable-static --enable-gpl --enable-version3"
 }
--- variants/win64-gpl.sh
+++ variants/win64-gpl.sh
@@ -2,6 +2,12 @@
 source "$(dirname "${BASH_SOURCE}")"/windows-install-static.sh
 source "$(dirname "${BASH_SOURCE}")"/defaults-gpl.sh
 
+# Сохраняем предыдущую версию функции, если она была (например из windows-install-static)
+eval "prev_ffbuild_configure_gpl() { $(declare -f ffbuild_configure | sed '1,2d;$d'); }"
+
 ffbuild_configure() {
+    # Вызываем "родителя"
+    prev_ffbuild_configure_gpl 2>/dev/null || true
+    # Добавляем свои флаги
     echo "--pkg-config-flags=\"--static\" --disable-shared --enable-static --enable-gpl --enable-version3"
 }
