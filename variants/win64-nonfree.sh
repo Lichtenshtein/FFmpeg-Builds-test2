f2a1c9cf43bc144ba45bff9a2a316efe79bb00a4
Lichtenshtein <altruumann@protonmail.com>
2026-04-29 13:27:23 +0300

    test libtensorflow

--- variants/win64-nonfree.sh
+++ variants/win64-nonfree.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 source "$(dirname "${BASH_SOURCE}")"/win64-gpl.sh
+
+# Бэкапим функцию из GPL скрипта
+eval "prev_ffbuild_configure_nonfree() { $(declare -f ffbuild_configure | sed '1,2d;$d'); }"
+
 ffbuild_configure() {
+    # Сначала печатаем всё, что было в GPL (и выше)
+    prev_ffbuild_configure_nonfree 2>/dev/null || true
+    # Добавляем nonfree
     echo "--enable-nonfree"
 }
 LICENSE_FILE=""
