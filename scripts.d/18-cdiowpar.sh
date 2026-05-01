b806ee4171aa4cd9add90dd98377e1f378a1b705
Lichtenshtein <altruumann@protonmail.com>
2026-05-01 06:47:45 +0300

    test full build

--- scripts.d/18-cdiowpar.sh
+++ scripts.d/18-cdiowpar.sh
@@ -13,28 +13,6 @@ ffbuild_dockerdl() {
 
 ffbuild_dockerbuild() {
     set -e
-    # заменяем реализацию gettime в utils.c на пустую/совместимую
-    # Это уберет ошибку компиляции, так как clock_gettime больше не будет вызываться
-    # echo "Patching utils.c to bypass clock_gettime..."
-    
-    # cat <<EOF > lib/cdda_interface/utils.c.patch
-# --- lib/cdda_interface/utils.c
-# +++ lib/cdda_interface/utils.c
-# @@ -176,11 +176,7 @@
- # static void
- # gettime (struct timespec *ts)
- # {
-# -#if defined(HAVE_CLOCK_GETTIME) && defined(CLOCK_MONOTONIC)
-# -  static clockid_t clock = (clockid_t)-1;
-# -  if ((int)clock == -1) clock = (clock_gettime(CLOCK_MONOTONIC, ts) < 0 ? CLOCK_REALTIME : CLOCK_MONOTONIC);
-# -  clock_gettime(clock, ts);
-# -#elif defined(HAVE_GETTIMEOFDAY)
-# +#if defined(HAVE_GETTIMEOFDAY)
-   # struct timeval tv;
-   # gettimeofday (&tv, NULL);
-   # ts->tv_sec = tv.tv_sec;
-# EOF
-    # patch -p0 < lib/cdda_interface/utils.c.patch
 
     local DEP_LIBS="-liconv -lcharset"
 
