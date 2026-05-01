93116a206e9a79de601274d4bad661dd1bb005e7
Lichtenshtein <altruumann@protonmail.com>
2026-05-01 06:37:37 +0300

    test 18-cdiowpar

--- scripts.d/18-cdiowpar.sh
+++ scripts.d/18-cdiowpar.sh
@@ -15,26 +15,28 @@ ffbuild_dockerbuild() {
     set -e
     # заменяем реализацию gettime в utils.c на пустую/совместимую
     # Это уберет ошибку компиляции, так как clock_gettime больше не будет вызываться
-    echo "Patching utils.c to bypass clock_gettime..."
+    # echo "Patching utils.c to bypass clock_gettime..."
     
-    cat <<EOF > lib/cdda_interface/utils.c.patch
---- lib/cdda_interface/utils.c
-+++ lib/cdda_interface/utils.c
-@@ -176,11 +176,7 @@
- static void
- gettime (struct timespec *ts)
- {
--#if defined(HAVE_CLOCK_GETTIME) && defined(CLOCK_MONOTONIC)
--  static clockid_t clock = (clockid_t)-1;
--  if ((int)clock == -1) clock = (clock_gettime(CLOCK_MONOTONIC, ts) < 0 ? CLOCK_REALTIME : CLOCK_MONOTONIC);
--  clock_gettime(clock, ts);
--#elif defined(HAVE_GETTIMEOFDAY)
-+#if defined(HAVE_GETTIMEOFDAY)
-   struct timeval tv;
-   gettimeofday (&tv, NULL);
-   ts->tv_sec = tv.tv_sec;
-EOF
-    patch -p0 < lib/cdda_interface/utils.c.patch
+    # cat <<EOF > lib/cdda_interface/utils.c.patch
+# --- lib/cdda_interface/utils.c
+# +++ lib/cdda_interface/utils.c
+# @@ -176,11 +176,7 @@
+ # static void
+ # gettime (struct timespec *ts)
+ # {
+# -#if defined(HAVE_CLOCK_GETTIME) && defined(CLOCK_MONOTONIC)
+# -  static clockid_t clock = (clockid_t)-1;
+# -  if ((int)clock == -1) clock = (clock_gettime(CLOCK_MONOTONIC, ts) < 0 ? CLOCK_REALTIME : CLOCK_MONOTONIC);
+# -  clock_gettime(clock, ts);
+# -#elif defined(HAVE_GETTIMEOFDAY)
+# +#if defined(HAVE_GETTIMEOFDAY)
+   # struct timeval tv;
+   # gettimeofday (&tv, NULL);
+   # ts->tv_sec = tv.tv_sec;
+# EOF
+    # patch -p0 < lib/cdda_interface/utils.c.patch
+
+    local DEP_LIBS="-liconv -lcharset"
 
     autoreconf -if
 
@@ -55,8 +57,8 @@ EOF
     CFLAGS="$CFLAGS ${USELTO}" \
     CPPFLAGS="$CPPFLAGS" \
     CXXFLAGS="$CXXFLAGS ${USELTO}" \
-    LDFLAGS="$LDFLAGS ${USELTO}" \
-    LIBS="$LIBS" \
+    LDFLAGS="$LDFLAGS $DEP_LIBS ${USELTO}" \
+    LIBS="$LIBS $DEP_LIBS" \
     ./configure "${myconf[@]}" || return 1
 
     make -j$(nproc) $MAKE_V || return 1
