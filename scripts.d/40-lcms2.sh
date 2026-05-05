37e865d6148e33d8e5078f4f9bd0e8564912b4c0
Lichtenshtein <altruumann@protonmail.com>
2026-05-05 16:04:20 +0300

    moderating our appetites for LTO to walkaround gcc 15 critical bug in ICE

--- scripts.d/40-lcms2.sh
+++ scripts.d/40-lcms2.sh
@@ -30,7 +30,7 @@ ffbuild_dockerbuild() {
         -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX"
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         -DCMAKE_POLICY_DEFAULT_CMP0069=NEW
-        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=$([ "${USE_LTO}" == "1" ] && echo ON || echo OFF)
+        # -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=$([ "${USE_LTO}" == "1" ] && echo ON || echo OFF)
         -DLCMS2_BUILD_SHARED=$([ "${PREFER_SHARED}" == "1" ] && echo ON || echo OFF)
         -DLCMS2_BUILD_STATIC=$([ "${PREFER_SHARED}" == "1" ] && echo OFF || echo ON)
         -DLCMS2_BUILD_TOOLS=OFF
