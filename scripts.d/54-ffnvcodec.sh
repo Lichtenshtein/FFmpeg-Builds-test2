f59a9ea59d3e939fad0a8e2139800efd5723f0ac
Lichtenshtein <altruumann@protonmail.com>
2026-05-05 09:10:49 +0300

    test ffnvcodec

--- scripts.d/54-ffnvcodec.sh
+++ scripts.d/54-ffnvcodec.sh
@@ -11,6 +11,8 @@ SCRIPT_REPO4="https://github.com/FFmpeg/nv-codec-headers.git"
 SCRIPT_COMMIT4="09e12e3d803ce79c327a9709233e8cd858e59d9e"
 SCRIPT_BRANCH4="sdk/8.1"
 
+export SKIP_POST_PATCH=1
+
 ffbuild_enabled() {
     [[ $TARGET == winarm64 ]] && return 1
     return 0
@@ -34,25 +36,24 @@ ffbuild_dockerbuild() {
     # ffnvcodec - это просто заголовки, Makefile простой.
     make PREFIX="$FFBUILD_PREFIX" DESTDIR="$FFBUILD_DESTDIR" install || return 1
 
-#     # Fallback для stdbit.h (C23), если компилятор его не находит
-#     # Проверяем через сам компилятор, видит ли он файл в своих путях
-#     if ! echo "#include <stdbit.h>" | $CC -E - >/dev/null 2>&1; then
-#         log_warn "stdbit.h not found in default paths. Creating a fallback stub..."
-#         mkdir -p "$INSTALL_ROOT/include"
-#         # Минимально необходимый контент для FFmpeg (обертки над built-ins)
-#         cat <<EOF > "$INSTALL_ROOT/include/stdbit.h"
-# #ifndef _STDBIT_H
-# #define _STDBIT_H
-# // Fallback stubs for C23 bit operations using GCC built-ins
-# #define __STDC_VERSION_STDBIT_H__ 202311L
-# #define stdc_count_ones(x) __builtin_popcountll((unsigned long long)(x))
-# #define stdc_trailing_zeros(x) __builtin_ctzll((unsigned long long)(x))
-# #define stdc_leading_zeros(x) __builtin_clzll((unsigned long long)(x))
-# #endif
-# EOF
-#     else
-#         log_info "stdbit.h is natively supported by $CC, no stub needed."
-#     fi
+     # Fallback для stdbit.h (C23), если компилятор его не находит
+     # Проверяем через сам компилятор, видит ли он файл в своих путях
+     if ! echo "#include <stdbit.h>" | $CC -E - >/dev/null 2>&1; then
+         log_warn "stdbit.h not found in default paths. Creating a fallback stub..."
+         mkdir -p "$INSTALL_ROOT/include"
+         # Минимально необходимый контент для FFmpeg (обертки над built-ins)
+         cat <<EOF > "$INSTALL_ROOT/include/stdbit.h"
+ #ifndef _STDBIT_H
+ #define _STDBIT_H
+ #define __STDC_VERSION_STDBIT_H__ 202311L
+ #define stdc_count_ones(x) __builtin_popcountll((unsigned long long)(x))
+ #define stdc_trailing_zeros(x) __builtin_ctzll((unsigned long long)(x))
+ #define stdc_leading_zeros(x) __builtin_clzll((unsigned long long)(x))
+ #endif
+ EOF
+     else
+         log_info "stdbit.h is natively supported by $CC, no stub needed."
+     fi
 
     local PC_FILE="$PC_DIR/ffnvcodec.pc"
     if [ -f "$PC_FILE" ]; then
