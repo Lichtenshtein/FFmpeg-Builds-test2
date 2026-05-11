287288602751014b846850c5a6a99100d431661c
Lichtenshtein <altruumann@protonmail.com>
2026-05-11 11:21:42 +0300

    test frei0r

--- scripts.d/84-frei0r.sh
+++ scripts.d/84-frei0r.sh
@@ -39,31 +39,11 @@ ffbuild_dockerbuild() {
     sed -i '/add_subdirectory (test)/d' CMakeLists.txt
 
 # -lgavl -lhogweed -lgmp -lnettle -lgnutls 
-    # Используем переменную CMAKE_CXX_STANDARD_LIBRARIES
-    # CMake ставит её в самый конец
-    # local DEP_LIBS="-Wl,--start-group
-    local DEP_LIBS=" \
--lopencv_features2d4140 -lopencv_dnn -lopencv_photo -lopencv_imgcodecs4140 -lopencv_highgui4140 -lopencv_video -lopencv_highgui -lopencv_objdetect4140 -lopencv_imgproc4140 -lopencv_ml -lopencv_videoio -lopencv_gapi4140 -lopencv_calib3d4140 -lopencv_video4140 -lopencv_dnn4140 -lopencv_flann -lopencv_core4140 -lopencv_core -lopencv_imgproc -lopencv_ml4140 -lopencv_stitching -lopencv_features2d -lopencv_gapi -lopencv_flann4140 -lopencv_imgcodecs -lopencv_calib3d -lopencv_photo4140 -lopencv_objdetect -lopencv_videoio4140 -lopencv_stitching4140 \
--lmsvc_stub -lippicv -lprotobuf -lade -lIlmImf -lipphal -lippiw \
--ljxl -ljxl_cms -ljxl_threads -lavif -lhwy -lopenvino \
--ltbb12 \
--lcairo-gobject -lcairo \
--lharfbuzz-icu -lharfbuzz-subset -lharfbuzz-cairo \
--lharfbuzz-vector -lharfbuzz-raster -lharfbuzz \
--lfontconfig -lfreetype -lpixman-1 -lfribidi \
--lgio-2.0 -lgthread-2.0 -lglib-2.0 \
--ltiffxx -ltiff -lopenjp2 -lturbojpeg -ljpeg -lpng16 -lgif \
--lwebpmux -lwebpdemux -lwebp -lwebpdecoder -lsharpyuv \
--llcms2_fast_float -llcms2_threaded -llcms2 \
--lxml2 -lpcre2-posix -lpcre2-8 -lffi -ljbig \
--lzstd -llzma \
--lbrotlienc -lbrotlidec -lbrotlicommon \
--lbz2 -lz \
--lintl -liconv -lcharset \
--lsicuin -lsicuuc -lsicudt \
--lglut \
--lstdc++ $LIBS $ADDITIONAL_LIBS"
-# -Wl,--end-group"
+    # Формируем список либ через pkg-config (так надежнее)
+    local DEP_LIBS=$(pkgconf --static --libs opencv4 cairo)
+    # Системные либы Windows, которые часто теряются
+    local WIN_SYS_LIBS="lcomdlg32 -lshell32 -loleaut32 -lcomctl32"
+    local RAW_LDFLAGS=$(echo "$LDFLAGS" | sed 's/-Wl,-Bstatic\b//g')
 
     mkdir build && cd build
 
@@ -78,17 +58,17 @@ ffbuild_dockerbuild() {
         -DWITHOUT_CAIRO=OFF  # required for cairo- filters and mixers
         -DWITHOUT_GAVL=ON   # required for scale0tilt and vectorscope filters
         # -DOPENCV_DIR="$FFBUILD_PREFIX/lib/cmake/opencv4"
-        # -DCMAKE_C_STANDARD_LIBRARIES="${DEP_LIBS}"
-        # -DCMAKE_CXX_STANDARD_LIBRARIES="${DEP_LIBS}"
+        -DCMAKE_C_STANDARD_LIBRARIES="${DEP_LIBS} ${WIN_SYS_LIBS} ${LIBS} ${ADDITIONAL_LIBS}"
+        -DCMAKE_CXX_STANDARD_LIBRARIES="${DEP_LIBS} -lstdc++ ${WIN_SYS_LIBS} ${LIBS} ${ADDITIONAL_LIBS}"
     )
 
     export static_flags=""
     [[ "${PREFER_SHARED}" != "1" ]] && static_flags="-DCAIRO_WIN32_STATIC_BUILD -Dpixman_static -DHARFBUZZ_STATIC -DFT2_BUILD_LIBRARY -DLIBXML_STATIC -DXML_STATIC"
 
-    CFLAGS="$CFLAGS $CPPFLAGS ${USELTO}${USELTO_C} $static_flags" \
-    CXXFLAGS="$CXXFLAGS $CPPFLAGS ${USELTO}${USELTO_C} $static_flags" \
-    LDFLAGS="$LDFLAGS ${USELTO}" \
-    LIBS="$DEP_LIBS" \
+# -Wl,--unresolved-symbols=ignore-all
+    CFLAGS="$CFLAGS $CPPFLAGS ${NOLTO} -fcommon $static_flags" \
+    CXXFLAGS="$CXXFLAGS $CPPFLAGS ${NOLTO} -fcommon $static_flags" \
+    LDFLAGS="$RAW_LDFLAGS ${NOLTO} -Wl,--allow-multiple-definition" \
     cmake -G Ninja "${myconf[@]}" .. || return 1
 
     ninja $NINJA_V || return 1
