bec32492f96797232a8dcdd7baa896a087db5648
Lichtenshtein <altruumann@protonmail.com>
2026-05-11 09:12:51 +0300

    test frei0r

--- scripts.d/84-frei0r.sh
+++ scripts.d/84-frei0r.sh
@@ -41,7 +41,8 @@ ffbuild_dockerbuild() {
 # -lgavl -lhogweed -lgmp -lnettle -lgnutls 
     # Используем переменную CMAKE_CXX_STANDARD_LIBRARIES
     # CMake ставит её в самый конец
-    local DEP_LIBS="-Wl,--start-group \
+    # local DEP_LIBS="-Wl,--start-group
+    local DEP_LIBS=" \
 -lopencv_features2d4140 -lopencv_dnn -lopencv_photo -lopencv_imgcodecs4140 -lopencv_highgui4140 -lopencv_video -lopencv_highgui -lopencv_objdetect4140 -lopencv_imgproc4140 -lopencv_ml -lopencv_videoio -lopencv_gapi4140 -lopencv_calib3d4140 -lopencv_video4140 -lopencv_dnn4140 -lopencv_flann -lopencv_core4140 -lopencv_core -lopencv_imgproc -lopencv_ml4140 -lopencv_stitching -lopencv_features2d -lopencv_gapi -lopencv_flann4140 -lopencv_imgcodecs -lopencv_calib3d -lopencv_photo4140 -lopencv_objdetect -lopencv_videoio4140 -lopencv_stitching4140 \
 -lmsvc_stub -lippicv -lprotobuf -lade -lIlmImf -lipphal -lippiw \
 -ljxl -ljxl_cms -ljxl_threads -lavif -lhwy -lopenvino \
@@ -61,8 +62,8 @@ ffbuild_dockerbuild() {
 -lintl -liconv -lcharset \
 -lsicuin -lsicuuc -lsicudt \
 -lglut \
--lstdc++ $LIBS $ADDITIONAL_LIBS \
--Wl,--end-group"
+-lstdc++ $LIBS $ADDITIONAL_LIBS"
+# -Wl,--end-group"
 
     mkdir build && cd build
 
@@ -77,8 +78,8 @@ ffbuild_dockerbuild() {
         -DWITHOUT_CAIRO=OFF  # required for cairo- filters and mixers
         -DWITHOUT_GAVL=ON   # required for scale0tilt and vectorscope filters
         # -DOPENCV_DIR="$FFBUILD_PREFIX/lib/cmake/opencv4"
-        -DCMAKE_C_STANDARD_LIBRARIES="${DEP_LIBS}"
-        -DCMAKE_CXX_STANDARD_LIBRARIES="${DEP_LIBS}"
+        # -DCMAKE_C_STANDARD_LIBRARIES="${DEP_LIBS}"
+        # -DCMAKE_CXX_STANDARD_LIBRARIES="${DEP_LIBS}"
     )
 
     export static_flags=""
