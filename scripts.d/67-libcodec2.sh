83a549e97e97ee1e32dcf391424e5bee7bf05bbb
Lichtenshtein <altruumann@protonmail.com>
2026-05-14 16:50:20 +0300

    test codec2

--- scripts.d/67-libcodec2.sh
+++ scripts.d/67-libcodec2.sh
@@ -30,12 +30,13 @@ ffbuild_dockerbuild() {
     sed -i 's|add_subdirectory(src)|# add_subdirectory(src)|g' CMakeLists.txt
     sed -i 's|add_subdirectory(unittest)|# add_subdirectory(unittest)|g' CMakeLists.txt
 
-    # Переносим сборку только самой библиотеки в корень
+    # Переносим сборку только самой библиотеки в корень с жесткой фильтрацией тестов
     cat << 'EOF' >> CMakeLists.txt
 # Ручное описание сборки только статической библиотеки libcodec2
 file(GLOB CODEC2_SRCS "src/*.c")
-# Исключаем генератор кодовых книг и файлы тестов
-list(FILTER CODEC2_SRCS EXCLUDE REGEX "generate_codebook.c|mac.c|vdec.c|venc.c")
+
+# Исключаем генератор кодовых книг, тестовые файлы (*_test.c) и исполняемые утилиты
+list(FILTER CODEC2_SRCS EXCLUDE REGEX "generate_codebook\\.c|mac\\.c|vdec\\.c|venc\\.c|.*_test\\.c|c2enc\\.c|c2dec\\.c|freedv_.*\\.c")
 
 add_library(codec2 STATIC ${CODEC2_SRCS})
 target_include_directories(codec2 PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/src" "${CMAKE_CURRENT_BINARY_DIR}")
