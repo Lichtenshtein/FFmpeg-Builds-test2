8869c7f97a6bdb10e9130008d1a9e4b07c715ec9
Lichtenshtein <altruumann@protonmail.com>
2026-05-16 23:27:14 +0300

    test building openvino from source

--- scripts.d/59-openvino.sh
+++ scripts.d/59-openvino.sh
@@ -23,6 +23,16 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     set -e
 
+    # Исключаем сборку папки демонстрационных сниппетов из документации
+    if [ -f "docs/CMakeLists.txt" ]; then
+        sed -i 's/\badd_subdirectory(snippets)\b/# add_subdirectory(snippets)/g' docs/CMakeLists.txt
+    fi
+
+    log_info "Fixing case-sensitive Shlwapi library names for Linux host..."
+    sed -i 's/\bShlwapi\b/shlwapi/g' src/common/util/CMakeLists.txt
+
+    export TBBROOT="$FFBUILD_PREFIX"
+
     mkdir build && cd build
 
     local myconf=(
@@ -63,11 +73,12 @@ ffbuild_dockerbuild() {
         -DENABLE_SAMPLES=OFF
         -DENABLE_TESTS=OFF
         -DENABLE_FUNCTIONAL_TESTS=OFF
+        -DENABLE_SNIPPETS_LIBXSMM_TPP=OFF
         -DENABLE_PYTHON=OFF
         -DENABLE_WHEEL=OFF
         -DENABLE_CLANG_FORMAT=OFF
         -DENABLE_PROFILING_ITT=OFF
-        # Включаем аппаратные инструкции вашего Xeon E5 2690v4 (Broadwell)
+        # Включаем аппаратные инструкции
         -DENABLE_SSE42=ON
         -DENABLE_AVX2=ON
         -DENABLE_AVX512F=$([ "${USE_AVX512}" == "1" ] && echo ON || echo OFF)
@@ -77,13 +88,15 @@ ffbuild_dockerbuild() {
         -DENABLE_INTEL_ITT=OFF # убирает привязку к Windows ITT/VTune API
         -DENABLE_CPPLINT=OFF # не ищет python-линтеры
         -DENABLE_NCC_STYLE=OFF # Отключает проверку стилей OpenVINO
+        # Дополнительно
         -DCMAKE_CXX_STANDARD=20
         -DCMAKE_CXX_STANDARD_REQUIRED=ON
+        -DCMAKE_COMPILE_WARNING_AS_ERROR=OFF
     )
 
     CFLAGS="$CFLAGS $CPPFLAGS ${USELTO}${USELTO_C}" \
     CXXFLAGS="$CXXFLAGS $CPPFLAGS ${USELTO}${USELTO_C}" \
-    LDFLAGS="$LDFLAGS ${USELTO}" \
+    LDFLAGS="$LDFLAGS ${USELTO} -Wl,--allow-multiple-definition" \
     cmake -G Ninja "${myconf[@]}" .. || return 1
 
     DESTDIR="$FFBUILD_DESTDIR" ninja install || return 1
@@ -95,10 +108,10 @@ ffbuild_dockerbuild() {
 # includedir=\${prefix}/include
 
 # Name: OpenVINO
-# Description: Intel OpenVINO Runtime (Static MinGW Broadwell Build)
+# Description: Intel OpenVINO Runtime (Static MinGW Broadwell Clean Build)
 # Version: 2025.4.1
-# Libs: -L\${libdir} -lopenvino -lopenvino_intel_cpu_plugin -lopenvino_ir_frontend
-# Libs.private: -ltbb -lstdc++ -lshlwapi
+# Libs: -L\${libdir} -lopenvino -lopenvino_intel_cpu_plugin -lopenvino_ir_frontend -lopenvino_shape_inference -lopenvino_onednn_cpu -lopenvino_common_translators -lopenvino_reference -lopenvino_util -lpugixml
+# Libs.private: -ltbb12 -lstdc++ -lshlwapi -lws2_32
 # Cflags: -I\${includedir} -I\${includedir}/openvino
 # EOF
 }
