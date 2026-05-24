90b3a4933542f5fc65ce47e3cbfd9196096308e5
Lichtenshtein <altruumann@protonmail.com>
2026-05-24 17:07:07 +0300

    test librsvg

--- util/vars.sh
+++ util/vars.sh
@@ -216,8 +216,8 @@ if [[ "$USE_LTO" == "1" ]]; then
 # Tip: rust has -C linker-plugin-lto LLVM Bitcode or LLVM MinGW
 # -fno-use-linker-plugin
     export RUSTLTO=" -C lto=fat" # thin
-    export USELTO="-flto=auto -flto-partition=max -fno-stack-clash-protection -fno-toplevel-reorder" # try 8
-    export USELTO_C=" -ffat-lto-objects -flto-compression-level=3 -fno-omit-frame-pointer -Wno-stringop-overflow -Wno-attributes -Wno-inline" # try 9
+    export USELTO="-flto=auto -flto-partition=balanced -fno-stack-clash-protection -fno-toplevel-reorder" # try 8
+    export USELTO_C=" -ffat-lto-objects -flto-compression-level=3 -fno-omit-frame-pointer -Wno-stringop-overflow -Wno-inline" # try 9
     export NOLTO="-fno-lto"
     export AR="${FFBUILD_TOOLCHAIN}-gcc-ar"
     export NM="${FFBUILD_TOOLCHAIN}-gcc-nm"
@@ -266,7 +266,7 @@ export HOST_CPPFLAGS="-D_FORTIFY_SOURCE=2"
 
 # Ветвление по TARGET
 if [[ "$TARGET" == "win64" ]]; then
-    export BASE_CFLAGS="-mms-bitfields -fstack-protector-strong"
+    export BASE_CFLAGS="-mms-bitfields -fstack-protector-strong -Wno-attributes"
     export BASE_CPPFLAGS="-D__USE_MINGW_ANSI_STDIO=1 -U_WIN32_WINNT -D_WIN32_WINNT=0x0A00 -D_WIN32 -D_FORTIFY_SOURCE=2"
 
     BASE_LD_FLAGS=(
@@ -310,7 +310,7 @@ if [[ "$TARGET" == "win64" ]]; then
     export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER="${FFBUILD_CROSS_PREFIX}gcc"
 
 elif [[ "$TARGET" == "linux64" ]]; then
-    export BASE_CFLAGS="-fstack-protector-strong"
+    export BASE_CFLAGS="-fstack-protector-strong -Wno-attributes"
     export BASE_CPPFLAGS="-D_FORTIFY_SOURCE=2"
 
     # Используем Linux-специфичные LDFLAGS
