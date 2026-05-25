7c79930289279914efcb782fd0a2da629ba51733
Lichtenshtein <altruumann@protonmail.com>
2026-05-25 03:21:52 +0300

    fix

--- build.sh
+++ build.sh
@@ -456,7 +456,13 @@ TOTAL_VIRTUAL=$(( MEM_PHYS + SWAP_TOTAL ))
 if [[ "$FINAL_CONFIGURE" =~ --enable-lto ]] || [[ "$USE_LTO" == "1" ]]; then
 
     # Force disable LTO for the problematic VVC module to prevent GCC choose_baseaddr ICE
-    sed -i 's/X86/X86\n#ifndef __clang__\n__attribute__((optimize("no-lto")))\n#endif/' libavcodec/vvc/intra_template.c
+    echo "CFLAGS-libavcodec/vvc/inter_template.o += -fno-lto" >> libavcodec/Makefile
+    echo "CFLAGS-libavcodec/vvc/intra_template.o += -fno-lto" >> libavcodec/Makefile
+    echo "CFLAGS-libavcodec/vvc/dsp.o += -fno-lto" >> libavcodec/Makefile
+
+    # На всякий случай отключаем LTO для всего каталога vvc
+    echo "CFLAGS-libavcodec/vvc/vvcdsp.o += -fno-lto" >> libavcodec/Makefile
+    echo "CFLAGS-libavcodec/vvc/vvcdec.o += -fno-lto" >> libavcodec/Makefile
 
     # защита таблиц MLP от удаления оптимизатором LTO (Dead Code Elimination)
     if [ -f "libavcodec/mlp.c" ]; then
@@ -539,7 +545,7 @@ CONF_FLAGS=(
     --host-ldflags="$HOST_LDFLAGS"
     --extra-cflags="${FINAL_CFLAGS}${ASAN_CFLAGS}"
     --extra-cxxflags="${FINAL_CXXFLAGS}${ASAN_CXXFLAGS}"
-    --extra-ldflags="${ASAN_LDFLAGS}${FINAL_LDFLAGS} -Wl,--allow-multiple-definition"
+    --extra-ldflags="${ASAN_LDFLAGS}${FINAL_LDFLAGS} -march=${CPU_ARCH} -mtune=${CPU_TUNE} -mavx2 -mfma -Wl,--allow-multiple-definition"
     --extra-ldexeflags="$FINAL_LDEXEFLAGS"
     --extra-libs="${FINAL_LIBS_GROUPED}"
     "${FF_CONF_ARR[@]}"
