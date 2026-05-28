7139ac654b74101cc35adb6920a072f47e886ee2
Lichtenshtein <altruumann@protonmail.com>
2026-05-28 03:07:54 +0300

    update ffmpeg header cleaner patch

--- build.sh
+++ build.sh
@@ -541,31 +541,22 @@ if ! ./configure "${CONF_FLAGS[@]}" 2>"$FFMPEG_CONFIG_LOG"; then
 fi
 
 # Очистка вывода конфигурации в самом бинарнике FFmpeg
-if [ -d "ffbuild" ]; then
-    log_info "Cleaning up and shortening the configuration string in build artifacts..."
+if [ -f "libavutil/ffversion.h" ]; then
+    log_info "Safe cleanup of the configuration string in libavutil/ffversion.h..."
 
-    # Список файлов, где хранится строка конфигурации
-    TARGET_FILES=("ffbuild/config.sh" "ffbuild/config.mak" "libavutil/ffversion.h")
+    # Модифицируем исключительно строку FFMPEG_CONFIGURATION в заголовочном файле
+    sed -i -E '/#define FFMPEG_CONFIGURATION/s/--extra-libs=[^"]*//g' libavutil/ffversion.h
+    sed -i -E '/#define FFMPEG_CONFIGURATION/s/--extra-cflags=[^"]*//g' libavutil/ffversion.h
+    sed -i -E '/#define FFMPEG_CONFIGURATION/s/--extra-cxxflags=[^"]*//g' libavutil/ffversion.h
+    sed -i -E '/#define FFMPEG_CONFIGURATION/s/--extra-ldflags=[^"]*//g' libavutil/ffversion.h
+    sed -i -E '/#define FFMPEG_CONFIGURATION/s/--extra-ldexeflags=[^"]*//g' libavutil/ffversion.h
+    sed -i -E '/#define FFMPEG_CONFIGURATION/s/--host-cflags=[^"]*//g' libavutil/ffversion.h
+    sed -i -E '/#define FFMPEG_CONFIGURATION/s/--host-ldflags=[^"]*//g' libavutil/ffversion.h
 
-    for file in "${TARGET_FILES[@]}"; do
-        if [ -f "$file" ]; then
-            # Вырезаем --extra-libs, --extra-cflags, --extra-cxxflags, --extra-ldflags, --extra-ldexeflags
-            # и --host-cflags, --host-ldflags вместе с их аргументами в любых кавычках
-            sed -i -E "s/--extra-libs=['\"][^'\"]*['\"]//g" "$file"
-            sed -i -E "s/--extra-cflags=['\"][^'\"]*['\"]//g" "$file"
-            sed -i -E "s/--extra-cxxflags=['\"][^'\"]*['\"]//g" "$file"
-            sed -i -E "s/--extra-ldflags=['\"][^'\"]*['\"]//g" "$file"
-            sed -i -E "s/--extra-ldexeflags=['\"][^'\"]*['\"]//g" "$file"
-            sed -i -E "s/--host-cflags=['\"][^'\"]*['\"]//g" "$file"
-            sed -i -E "s/--host-ldflags=['\"][^'\"]*['\"]//g" "$file"
-
-            # Схлопываем множественные пробелы, которые остались после удаления флагов
-            sed -i "s/  */ /g" "$file"
-            # Убираем пробелы перед закрывающими кавычками/апострофами, если они образовались
-            sed -i "s/ \(['\"]\)/\1/g" "$file"
-        fi
-    done
+    # Схлопываем лишние пробелы внутри кавычек
+    sed -i '/#define FFMPEG_CONFIGURATION/s/  */ /g' libavutil/ffversion.h
 fi
+
 
 # Сборка и установка ffmpeg
 make -j"$MAKE_JOBS" ${MAKE_V:+$MAKE_V}
