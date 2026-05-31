57c184a6196f0686a0a244b2e944ecc0f0fe3507
Lichtenshtein <altruumann@protonmail.com>
2026-06-01 00:41:22 +0300

    test lcevc

--- build.sh
+++ build.sh
@@ -212,7 +212,7 @@ FINAL_LIBS=$(smart_libs_dedupe "$LIBS" "$TOTAL_FF_LIBS" "$ADDITIONAL_LIBS" "$VAR
 # =======================================
 log_debug "${SEARCH_MARK} Scanning FFmpeg configuration for enabled components..."
 # Список компонентов для проверки
-COMPONENTS=(libtorch libopenvino libflite audiotoolbox libtensorflow libtesseract libfdk-aac openssl amf frei0r whisper)
+COMPONENTS=(libtorch libopenvino libflite audiotoolbox libtensorflow libtesseract libfdk-aac openssl amf frei0r whisper lcevcdec)
 # Создаем имя переменной libtesseract -> HAS_LIBTESSERACT
 for comp in "${COMPONENTS[@]}"; do
     clean_name="${comp^^}"
@@ -543,18 +543,23 @@ else
     log_error "Файл ${TARGET_PC_FILE} не найден! Проверьте правильность пути."
 fi
 
-log_info "Патчим libavfilter/vf_lcevc.c под сигнатуры LCEVC SDK 4.0.5..."
+if [[ "$HAS_LCEVCDEC" == "1" ]]; then
+    log_info "Patch libavfilter/vf_lcevc.c to support LCEVC SDK 4.0.5 signatures..."
 
-# 1. Исправляем LCEVC_SendDecoderEnhancementData
-# Старый вызов: LCEVC_SendDecoderEnhancementData(lcevc->decoder, in->pts, 0, sd->data, sd->size);
-# Новый вызов удаляет 3-й аргумент (0), сдвигая указатель и размер:
-sed -i 's/LCEVC_SendDecoderEnhancementData(lcevc->decoder, in->pts, 0, sd->data, sd->size)/LCEVC_SendDecoderEnhancementData(lcevc->decoder, in->pts, sd->data, sd->size)/g' libavfilter/vf_lcevc.c
+    # Исправляем LCEVC_SendDecoderEnhancementData
+    # Старый вызов: LCEVC_SendDecoderEnhancementData(lcevc->decoder, in->pts, 0, sd->data, sd->size);
+    # Новый вызов удаляет 3-й аргумент (0), сдвигая указатель и размер
+    sed -i 's/LCEVC_SendDecoderEnhancementData(lcevc->decoder, in->pts, 0, sd->data, sd->size)/LCEVC_SendDecoderEnhancementData(lcevc->decoder, in->pts, sd->data, sd->size)/g' libavfilter/vf_lcevc.c
 
-# 2. Исправляем LCEVC_SendDecoderBase
-# Старый вызов: LCEVC_SendDecoderBase(lcevc->decoder, in->pts, 0, picture, -1, in);
-# Сигнатура 4.0.5 ожидает 5 аргументов. Скорее всего, удаляется лишний флаг (0 или -1). 
-# Исходя из прототипа lcevc_dec.h, убираем внутренний ноль, чтобы picture встал на место base:
-sed -i 's/LCEVC_SendDecoderBase(lcevc->decoder, in->pts, 0, picture, -1, in)/LCEVC_SendDecoderBase(lcevc->decoder, in->pts, picture, -1, in)/g' libavfilter/vf_lcevc.c
+    # Исправляем LCEVC_SendDecoderBase
+    # Старый вызов: LCEVC_SendDecoderBase(lcevc->decoder, in->pts, 0, picture, -1, in);
+    # Сигнатура 4.0.5 ожидает 5 аргументов. Скорее всего, удаляется лишний флаг (0 или -1).
+    # Исходя из прототипа lcevc_dec.h, убираем внутренний ноль, чтобы picture встал на место base
+    sed -i 's/LCEVC_SendDecoderBase(lcevc->decoder, in->pts, 0, picture, -1, in)/LCEVC_SendDecoderBase(lcevc->decoder, in->pts, picture, -1, in)/g' libavfilter/vf_lcevc.c
+fi
+
+# Заменяем проверку #if HAVE_FCNTL на проверку, исключающую Windows
+[[ "$TARGET" == "win64" ]] && log_info "Patching libavformat/file.c: blocking HAVE_FCNTL for Windows..." && sed -i 's/#if HAVE_FCNTL/#if HAVE_FCNTL \&\& !defined(_WIN32)/g' libavformat/file.c
 
 
 [[ "$HAS_AUDIOTOOLBOX" == "0" ]] && CONF_FLAGS+=( --disable-audiotoolbox --disable-videotoolbox )
