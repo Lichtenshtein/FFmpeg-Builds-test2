9ede674f0aa7f0f6515675f9b445803080639854
Lichtenshtein <altruumann@protonmail.com>
2026-06-13 09:16:55 +0300

    update LTO function

--- addins/lto.sh
+++ addins/lto.sh
@@ -4,30 +4,20 @@ log_info "${XCLAM_MARK} LTO Addin: Enabling Link Time Optimization..."
 
 # Флаг для FFmpeg
 # --enable-lto=full (default); --enable-lto=thin (to save build time)
-
-# disabled LTO due to critical bug in gcc-15.2; ICE (Internal Compiler Error)
-# lto1: internal compiler error: in choose_baseaddr, at config/i386/i386.cc:7447
 ffbuild_configure() {
     echo "--enable-lto"
 }
 
 # -ffat-lto-objects позволит библиотекам содержать как LTO-код, так и обычный объектный код. Это увеличит размер промежуточных библиотек, но сделает линковку более стабильной
-# -mpreferred-stack-boundary=4
 ffbuild_cflags() {
     echo "${USELTO}${USELTO_C} -mstackrealign"
 }
 ffbuild_cxxflags() {
     echo "${USELTO}${USELTO_C} -mstackrealign"
 }
-# "-fno-use-linker-plugin"
 ffbuild_ldflags() {
     echo "${USELTO}"
 }
-
-# для LTO нужны версии с плагинами)
-# export AR="${FFBUILD_CROSS_PREFIX}gcc-ar"
-# export NM="${FFBUILD_CROSS_PREFIX}gcc-nm"
-# export RANLIB="${FFBUILD_CROSS_PREFIX}gcc-ranlib"
 
 for tool in AR NM RANLIB; do
     tool_path="${!tool}"
