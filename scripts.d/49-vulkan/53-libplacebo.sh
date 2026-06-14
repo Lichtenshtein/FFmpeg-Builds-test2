60870494ff7f435c7e0a128fa8e7fe61c5608311
Lichtenshtein <altruumann@protonmail.com>
2026-06-14 21:14:05 +0300

    test zimg libplacebo

--- scripts.d/49-vulkan/53-libplacebo.sh
+++ scripts.d/49-vulkan/53-libplacebo.sh
@@ -64,7 +64,7 @@ ffbuild_dockerbuild() {
         -Dopengl=disabled # OpenGL-based renderer
         -Dgl-proc-addr=disabled # Enable built-in OpenGL loader; uses dlopen, dlsym
         #-Dvk-proc-addr=enabled # Link directly against vkGetInstanceProcAddr from libvulkan.so
-        -Dvulkan-registry="$FFBUILD_PREFIX"/share/vulkan/registry/vk.xml
+        # -Dvulkan-registry="$FFBUILD_PREFIX"/share/vulkan/registry/vk.xml
         -Dvulkan-sdk="$FFBUILD_PREFIX"
         -Dvulkan=disabled
         -Dxxhash=disabled # faster replacement for internal siphash
@@ -77,7 +77,7 @@ ffbuild_dockerbuild() {
     fi
 
     local EXTRA_LDFLAGS="-lstdc++"
-    local EXTRA_CFLAGS="-I${FFBUILD_PREFIX}/include/spirv_cross"
+    # local EXTRA_CFLAGS="-I${FFBUILD_PREFIX}/include/spirv_cross"
 
     meson setup "${myconf[@]}" .. \
         -Dc_args="$CFLAGS $CPPFLAGS ${USELTO}${USELTO_C} $EXTRA_CFLAGS" \
