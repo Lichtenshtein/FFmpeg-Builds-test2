16812dfb7bdec7398c7323ba20eab713698dfcbc
Lichtenshtein <altruumann@protonmail.com>
2026-06-17 09:54:37 +0300

    test avisynth

--- scripts.d/80-avisynth.sh
+++ scripts.d/80-avisynth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export USE_VERS_FINDER=1
 SCRIPT_REPO="https://github.com/AviSynth/AviSynthPlus.git"
 SCRIPT_COMMIT="345a00034c6f167662119c129b06a1f65b72b619"
 
@@ -21,7 +21,25 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     set -e
 
+    # Отвязка от .git
+    cat << EOF > avs_core/Version.cmake
+set(AVS_SEQREV "4600")
+set(AVS_BRANCH "master")
+set(AVS_NEWEST_TAG "${VER_FULL}")
+set(AVS_DEVNEXT_REV "0")
+set(AVS_DEV_REVDATE "2026-06-17")
+set(AVS_DEV_GITHASH "ffbuild")
+set(ENV{SOURCE_DATE_EPOCH} "1773781200")
+
+if(NOT SRC OR NOT DST)
+    message(FATAL_ERROR "SRC or DST not set")
+endif()
+configure_file(${SRC} ${DST} @ONLY)
+EOF
+
     mkdir -p build && cd build
+
+    local extra_cxx_flags="-DRELEASE_TARBALL"
 
     local myconf=(
         -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN"
@@ -38,7 +56,7 @@ ffbuild_dockerbuild() {
     )
 
     CFLAGS="$CFLAGS $CPPFLAGS ${USELTO}${USELTO_C}" \
-    CXXFLAGS="$CXXFLAGS $CPPFLAGS ${USELTO}${USELTO_C}" \
+    CXXFLAGS="$CXXFLAGS $CPPFLAGS ${extra_cxx_flags} ${USELTO}${USELTO_C}" \
     LDFLAGS="$LDFLAGS ${USELTO}" \
     cmake -G Ninja "${myconf[@]}" .. || return 1
 
