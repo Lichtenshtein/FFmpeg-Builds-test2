aa758314bd31fb122f09431c964b5ce86a690f08
Lichtenshtein <altruumann@protonmail.com>
2026-06-21 21:14:55 +0300

    add another repo for libgsm

--- scripts.d/67-libgsm.sh
+++ scripts.d/67-libgsm.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 export USE_VERS_FINDER=1
-SCRIPT_REPO="https://github.com/maekawa-mugi/libgsm-playground.git"
-SCRIPT_COMMIT="f7fed756ba3c28792b876236cfbc69524ca048c7"
+SCRIPT_REPO="https://github.com/MartinEesmaa/libgsm.git"
+SCRIPT_COMMIT="0f915c8872786fed91bb67837e3ad0c7a7144c1e"
+
+# SCRIPT_REPO="https://github.com/maekawa-mugi/libgsm-playground.git"
+# SCRIPT_COMMIT="f7fed756ba3c28792b876236cfbc69524ca048c7"
+
+export SKIP_PRE_PATCH=1
 
 ffbuild_enabled() {
     return 0
