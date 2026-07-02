9fcecccb9293db0ca297209c5612f6fc58e495a3
Lichtenshtein <altruumann@protonmail.com>
2026-07-02 16:14:07 +0300

    fix host flags LTO variables

--- build.sh
+++ build.sh
@@ -615,7 +615,7 @@ CONF_FLAGS=(
     --host-ldflags="$HOST_LDFLAGS"
     --extra-cflags="${FINAL_CFLAGS}"
     --extra-cxxflags="${FINAL_CXXFLAGS}"
-    --extra-ldflags="${FINAL_LDFLAGS} -Wl,--allow-multiple-definition"
+    --extra-ldflags="${FINAL_LDFLAGS} -Wl,--allow-multiple-definition  -fuse-linker-plugin"
     --extra-ldexeflags="${FINAL_LDEXEFLAGS}"
     --extra-libs="${FINAL_LIBS_GROUPED}"
     "${FF_CONF_ARR[@]}"
