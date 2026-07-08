c00d538db59accf17bd7b68855ab9b8df25b8f53
Lichtenshtein <altruumann@protonmail.com>
2026-07-09 01:07:00 +0300

    fix libomnidrive commit

--- util/vars.sh
+++ util/vars.sh
@@ -294,7 +294,7 @@ should_apply_lto() {
             return 0
             ;;
         # прочее
-        *"cdio"|*"cdiowpar"|*"lensfun"|*"libaribb24"|*"libaribcaption"|*"libass"|*"zvbi"|*"qrencode"|*"quirc"|*"amf"|*"libklvanc"|*"vidstab"|*"libcaca"|*"libudfread"|*"libdvdcss"|*"libdvdread"|*"libdvdnav"|*"libbluray")
+        *"cdio"|*"cdiowpar"|*"lensfun"|*"libaribb24"|*"libaribcaption"|*"libass"|*"zvbi"|*"qrencode"|*"quirc"|*"amf"|*"libklvanc"|*"vidstab"|*"libcaca"|*"libudfread"|*"libdvdcss"|*"libdvdread"|*"libdvdnav"|*"libbluray"|*"libomnidrive")
             return 0
             ;;
         # Все остальные компоненты собираются БЕЗ LTO
