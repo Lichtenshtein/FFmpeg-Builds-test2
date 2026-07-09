50c0da37b30661deebd813346639c8f56396774f
Lichtenshtein <altruumann@protonmail.com>
2026-07-09 20:12:55 +0300

    generate dynamic cross-build toolchain files

--- images/base-linux64/toolchain.cmake
+++ images/base-linux64/toolchain.cmake
@@ -15,3 +15,21 @@ set(CMAKE_FIND_ROOT_PATH /opt/ct-ng /opt/ct-ng/${triple}/sysroot /opt/ffbuild)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# =============================================================================
+# GLOBAL VISIBILITY OVERRIDE (Forcing static links transparency)
+# =============================================================================
+
+# форсируем дефолтную (видимую) видимость символов в кэше
+# set(CMAKE_C_VISIBILITY_PRESET "default" CACHE INTERNAL "Global override" FORCE)
+# set(CMAKE_CXX_VISIBILITY_PRESET "default" CACHE INTERNAL "Global override" FORCE)
+# set(CMAKE_VISIBILITY_INLINES_HIDDEN 0 CACHE INTERNAL "Global override" FORCE)
+
+# Защита от переопределения свойств конкретных таргетов (set_target_properties)
+# CMake позволяет задать глобальное поведение для всех создаваемых таргетов по умолчанию
+# set(CMAKE_C_VISIBILITY_PRESET_INIT "default")
+# set(CMAKE_CXX_VISIBILITY_PRESET_INIT "default")
+# set(CMAKE_VISIBILITY_INLINES_HIDDEN_INIT 0)
+
+# set(CMAKE_STATIC_LIBRARY_CXX_FLAGS "${CMAKE_STATIC_LIBRARY_CXX_FLAGS} -fvisibility=default")
+# set(CMAKE_STATIC_LIBRARY_C_FLAGS "${CMAKE_STATIC_LIBRARY_C_FLAGS} -fvisibility=default")
