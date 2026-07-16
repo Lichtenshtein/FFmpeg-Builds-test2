3d59a238e545baf1c3675f4d6939cf9a553d5f68
Lichtenshtein <altruumann@protonmail.com>
2026-07-16 23:12:36 +0300

    test different linkers with ffmpeg itself

--- build.sh
+++ build.sh
@@ -612,6 +612,8 @@ chmod +x configure
 # FINAL_CFLAGS="${FINAL_CFLAGS// -mconsole/}"
 # FINAL_CXXFLAGS="${FINAL_CXXFLAGS// -mconsole/}"
 
+FINAL_LDFLAGS="${FINAL_LDFLAGS//-Wl,-plugin*/}"
+
 FINAL_LDFLAGS="${FINAL_LDFLAGS//-Wl,--stack,8388608/}"
 FINAL_LDFLAGS="${FINAL_LDFLAGS//--stack 8388608/}"
 FINAL_LDFLAGS="${FINAL_LDFLAGS//-Wl,--subsystem=console/}"
@@ -619,10 +621,6 @@ FINAL_LDFLAGS="${FINAL_LDFLAGS//-Wl,--high-entropy-va/}"
 FINAL_LDFLAGS="${FINAL_LDFLAGS//-Wl,--nxcompat/}"
 FINAL_LDFLAGS="${FINAL_LDFLAGS//-Wl,--dynamicbase/}"
 FINAL_LDFLAGS="${FINAL_LDFLAGS//-Wl,--gc-sections/}"
-
-SAFE_WIN_FLAGS="-Wl,--high-entropy-va,--nxcompat,--dynamicbase,--subsystem=console,--stack=8388608,--gc-sections,--entry=mainCRTStartup,--defsym,WinMain=main"
-
-FINAL_LDFLAGS="$(smart_dedupe "$LDFLAGS" "$TOTAL_FF_LDFLAGS" "$VARIANT_FF_LDFLAGS")"
 
 HOST_CFLAGS="${HOST_CFLAGS//-flto=4/}"
 HOST_CFLAGS="${HOST_CFLAGS//-flto-partition=balanced/}"
@@ -648,6 +646,8 @@ HOST_LDFLAGS="${HOST_LDFLAGS//-flto-partition=balanced/}"
 FINAL_LDFLAGS="${FINAL_LDFLAGS//-flto=4/}"
 FINAL_LDFLAGS="${FINAL_LDFLAGS//-flto-partition=balanced/}"
 
+SAFE_WIN_FLAGS="-Wl,--high-entropy-va,--nxcompat,--dynamicbase,--subsystem=console,--stack=8388608,--gc-sections,--entry=mainCRTStartup,--defsym=WinMain=main"
+
 
 # FINAL_LDFLAGS="${FINAL_LDFLAGS} -fno-use-linker-plugin"
 
@@ -664,7 +664,7 @@ CONF_FLAGS=(
     --host-ldflags="$HOST_LDFLAGS"
     --extra-cflags="${FINAL_CFLAGS}"
     --extra-cxxflags="${FINAL_CXXFLAGS}"
-    --extra-ldflags="${FINAL_LDFLAGS} ${SAFE_WIN_FLAGS} -Wl,--allow-multiple-definition"
+    --extra-ldflags="-L${FFBUILD_PREFIX}/lib ${FINAL_LDFLAGS} ${SAFE_WIN_FLAGS} -Wl,--allow-multiple-definition"
     --extra-ldexeflags="${FINAL_LDEXEFLAGS}"
     --extra-libs="${FINAL_LIBS_GROUPED}"
     --enable-runtime-cpudetect
