d8c87bf26bff79079feefa1de339ac0e715b32ec
Lichtenshtein <altruumann@protonmail.com>
2026-07-23 03:45:00 +0300

    test openblas

--- scripts.d/15-openblas.sh
+++ scripts.d/15-openblas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/OpenMathLib/OpenBLAS.git"
-SCRIPT_COMMIT="e0166008be8e466242aa76b2ff75ce3f0fbf574a"
+SCRIPT_COMMIT="7c991951a5f3b1dbdd27f200f2bd4557b4c51a3d"
 
 ffbuild_enabled() {
     return 0
